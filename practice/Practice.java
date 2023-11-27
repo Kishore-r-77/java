1ffc5d43d10d861a92ebf2a2a9a0bdc39305f07a
Kishore-r-77 <kishorework77@gmail.com>
2023-11-27 20:42:57 +0530

    practiced using functional foreach in array

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,44 +1,15 @@
 package practice;
 
-import practice.Animal;
-import practice.Cat;
-
-class Animal {
-	int age;
-	String name;
-
-	Animal(int age, String name) {
-		this.age = age;
-		this.name = name;
-	}
-
-	void display() {
-		System.out.println(name + "-" + age);
-	}
-	void show() {
-		System.out.println("Test");
-	}
-}
-
-class Cat extends Animal {
-	String category;
-
-	Cat(int age, String name, String category) {
-		super(age, name);
-		this.category = category;
-	}
-	void display() {
-		System.out.println(name+"-"+age+"-"+category);
-		show();
-	}
-
-}
+import java.util.Arrays;
 
 class Practice {
 
 	public static void main(String[] args) {
-		Animal cat = new Cat(4,"Ricky","Mammal");
+		int a[]=new int[1];
+		int b[]=a;
 		
-		cat.display();
+		a[0]=7;
+		
+		Arrays.stream(b).forEach(n->System.out.println(n));
 	}
 }
