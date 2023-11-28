23d4769fcd720f4adc900ab5b0398b135f5328b0
Kishore-r-77 <kishorework77@gmail.com>
2023-11-28 20:56:52 +0530

    practiced to String()

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,18 +1,24 @@
 package practice;
 
+class Person{
+	String name="Kishore";
+	void display() {
+		System.out.println("Hey this is "+this.name);
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+	
+	
+}
+
 class Practice {
 
 	public static void main(String[] args) {
 		
-		int n = 1;
-		
-		do {
-			System.out.println("Will run this atleast once");
-			n++;
-		}while(n!=5);
-		
-		
-		
-		
+		Person kishore = new Person();
+		System.out.println(kishore);
 	}
 }
