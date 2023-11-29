f6e00bd939be6795865a533e6e0130155d9165bd
Kishore-r-77 <kishorework77@gmail.com>
2023-11-29 20:39:49 +0530

    practiced varargs

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,28 +1,17 @@
 package practice;
 
+import java.util.Arrays;
+
 class Practice {
 
 	public static void main(String[] args) {
 
-		String day = "Wednesday";
-
-		switch (day) {
-		case "Monday" -> System.out.println("It's Monday");
-
-		case "Tuesday" -> System.out.println("It's Tuesday");
-
-		case "Wednesday" -> System.out.println("It's Wednesday");
-
-		case "Thursday" -> System.out.println("It's Thursday");
-
-		case "Friday" -> System.out.println("It's Friday");
-
-		case "Saturday" -> System.out.println("It's Saturday");
-
-		case "Sunday" -> System.out.println("It's Sunday");
-
-		default -> System.out.println("No day selected");
-		}
+	varargexample(1,4,5,62,3);
+		
+	}
+	
+	public static void varargexample(int ...x) {
+		System.out.println(Arrays.toString(x));
 	}
 
 }
