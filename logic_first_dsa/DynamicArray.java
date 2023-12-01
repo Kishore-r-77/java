e02d95692e7ad4d9bc469d20d51420c2dbbd8db7
Kishore-r-77 <kishorework77@gmail.com>
2023-12-01 13:53:06 +0530

    worked on insert at position in Array dsa

--- logic_first_dsa/DynamicArray.java
+++ logic_first_dsa/DynamicArray.java
@@ -21,6 +21,16 @@ class MyArrayList {
 		}
 		arr[size++] = val;
 	}
+	
+	void insertAtPos(int pos,int val) {
+		if(size==capacity) {
+			expandArray();
+		}
+		for(int i = size-1;i>=pos;i--)
+			arr[i+1]=arr[i];
+		arr[pos]=val;
+		size++;
+	}
 
 	void expandArray() {
 		capacity *= 2;
@@ -36,34 +46,55 @@ class MyArrayList {
 public class DynamicArray {
 
 	public static void main(String[] args) {
+		int inputVal, pos;
 		MyArrayList list = new MyArrayList();
-		System.out.println("------- List of Key Inputs for Performing the Provided Operations-------");
-		System.out.println("1.Enter 1 for Adding Value to the Array ");
-		System.out.println("5.Enter 5 for Exit");
+		
 		Scanner scanner = new Scanner(System.in);
-		int choice = scanner.nextInt();
+		
 
 		while (true) {
+			System.out.println("------- List of Key Inputs for Performing the Provided Operations-------");
+			System.out.println("1.Enter 1 for Adding Value at the end to an Array ");
+			System.out.println("2.Enter 2 to Display the Arrays");
+			System.out.println("3.Enter 3 to Insert value in an Array of your desired position.");
+			System.out.println("4.Enter 4 to Delete a value in an Array.");
+			System.out.println("5.Enter 5 for Exit");
+			System.out.println("\n------------------------------------------------------------------------\n");
+			System.out.println("Enter your're choice");
+			
+			int choice = scanner.nextInt();
 			switch (choice) {
 			case 1 -> {
 				System.out.println("Enter the Number to be Added in the Array");
-				int inputVal = scanner.nextInt();
+				inputVal = scanner.nextInt();
 				list.addValue(inputVal);
-				list.display();
 			}
-			case 2->{
+			case 2 ->{
 				list.display();
+				
 			}
+			case 3 -> {
+				System.out.println("Enter the Position");
+				pos=scanner.nextInt();
+				if(pos<0) {
+					System.out.println("Invalid Position");
+					break;
+				}
+				System.out.println("Enter the Value");
+				inputVal=scanner.nextInt();
+				list.insertAtPos(pos, inputVal);
+			}
+			
 			case 5 -> {
 				scanner.close();
 				System.exit(0);
-				
+
 			}
 			default -> {
 				System.out.println("Entered wrong Key");
 			}
 			}
-//			scanner.close();
+			
 
 		}
 
