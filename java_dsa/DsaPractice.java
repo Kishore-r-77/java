f35ffc2e59c9d4aae40255dac13985e114c00368
Kishore-r-77 <kishorework77@gmail.com>
2023-12-02 20:21:25 +0530

    practiced reversing of an array

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -4,17 +4,30 @@ import java.util.Arrays;
 
 public class DsaPractice {
 	public static void main(String[] args) {
+
+		int[] nums = { 1, 2, 3, 4, 5 };
+
+//		swapArray(nums, 1, 4);
 		
-		int[] nums = {1,2,3,4,5};
-		
-		swapArray(nums, 1, 4);
+		reverseArray(nums);
 		System.out.println(Arrays.toString(nums));
-		
+
 	}
-	
-	static void swapArray(int arr[],int pos1,int pos2) {
+
+	static void swapArray(int arr[], int pos1, int pos2) {
 		int temp = arr[pos1];
-		arr[pos1]=arr[pos2];
-		arr[pos2]=temp;
+		arr[pos1] = arr[pos2];
+		arr[pos2] = temp;
+	}
+
+	static void reverseArray(int arr[]) {
+		int start = 0;
+		int end = arr.length - 1;
+		while(start<end) {
+			swapArray(arr, start, end);
+			start++;
+			end--;
+		}
+		
 	}
 }
