506ec2f709b4e93dadc9ecba007f600dee415a01
Kishore-r-77 <kishorework77@gmail.com>
2023-12-02 20:28:46 +0530

    practiced reversing array based on Range

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -9,7 +9,8 @@ public class DsaPractice {
 
 //		swapArray(nums, 1, 4);
 		
-		reverseArray(nums);
+//		reverseArray(nums);
+		reverseArrayoOnRange(nums, 1, 4);
 		System.out.println(Arrays.toString(nums));
 
 	}
@@ -23,6 +24,23 @@ public class DsaPractice {
 	static void reverseArray(int arr[]) {
 		int start = 0;
 		int end = arr.length - 1;
+		while(start<end) {
+			swapArray(arr, start, end);
+			start++;
+			end--;
+		}
+		
+	}
+	
+	static void reverseArrayoOnRange(int arr[],int pos1, int pos2) {
+		int start = pos1;
+		int end = pos2;
+		if(start>end) {
+			System.out.println("Position cannot be Greater than Position 2");
+		}
+		if(end>arr.length-1) {
+			System.out.println("Index out of Bound Brother.");
+		}
 		while(start<end) {
 			swapArray(arr, start, end);
 			start++;
