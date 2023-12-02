6f5004963039043c12330c103a10cd3b8802d300
Kishore-r-77 <kishorework77@gmail.com>
2023-12-02 12:35:27 +0530

    learnt about null

--- kunal_dsa/ArrayDemo.java
+++ kunal_dsa/ArrayDemo.java
@@ -9,7 +9,13 @@ public class ArrayDemo {
 		for(int x:nums) {
 			System.out.println(x);
 		}
-
+		
+		String name=null;
+		Integer num=null;
+	
+//		null can't be assigned for primitives
+//		char a = null;
+//		int n = null;
 	}
 
 }
