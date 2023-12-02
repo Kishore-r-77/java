f42aaca9a0c2b37d70167900fcec5d0d80d728d3
Kishore-r-77 <kishorework77@gmail.com>
2023-12-02 20:06:30 +0530

    practiced swapping of numbers

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,7 +1,20 @@
 package java_dsa;
 
+import java.util.Arrays;
+
 public class DsaPractice {
 	public static void main(String[] args) {
-		System.out.println("Started Readyyyyy");
+		
+		int[] nums = {1,2,3,4,5};
+		
+		swapArray(nums, 1, 4);
+		System.out.println(Arrays.toString(nums));
+		
+	}
+	
+	static void swapArray(int arr[],int pos1,int pos2) {
+		int temp = arr[pos1];
+		arr[pos1]=arr[pos2];
+		arr[pos2]=temp;
 	}
 }
