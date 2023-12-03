4a539704b321a393f8480e8236b1c83fa3988e39
Kishore-r-77 <kishorework77@gmail.com>
2023-12-03 17:02:51 +0530

    changed the Function to BiPredicate

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,20 +1,21 @@
 package java_dsa;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		int nums[] = {1,4,2,5,66};
-		
-		System.out.println(search(nums, 66));
+		Integer nums[] = { 1, 4, 2, 5, 66 };
+
+		System.out.println(search.test(nums, 66));
 
 	}
-	
-	static boolean search(int[] nums,int val) {
-		return Arrays.stream(nums).anyMatch(n->n==val);	
-	}
 
+//	static boolean search(int[] nums,int val) {
+//		return Arrays.stream(nums).anyMatch(n->n==val);	
+//	}
 
-	
+	static BiPredicate<Integer[], Integer> search = (arr, val) -> Arrays.stream(arr).anyMatch(n -> n == val);
+
 }
