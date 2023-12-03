c4ecd70d5c74a20c3320d5bcf8c9464ebed1eb53
Kishore-r-77 <kishorework77@gmail.com>
2023-12-03 16:38:49 +0530

    practiced 2d array

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -3,61 +3,22 @@ package java_dsa;
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		int[] nums = { 1, 2, 31, 4, 5 };
-
-//		swapArray(nums, 1, 4);
+		int[][] nums = {
+				{1,2,3},
+				{4,5},
+				{6,7,8}
+		};
 		
-//		reverseArray(nums);
-//		reverseArrayoOnRange(nums, 1, 4);
-//		System.out.println(Arrays.toString(nums));
-		System.out.println(maxVal(nums));
-		
-
-	}
-
-	static void swapArray(int arr[], int pos1, int pos2) {
-		int temp = arr[pos1];
-		arr[pos1] = arr[pos2];
-		arr[pos2] = temp;
-	}
-
-	static void reverseArray(int arr[]) {
-		int start = 0;
-		int end = arr.length - 1;
-		while(start<end) {
-			swapArray(arr, start, end);
-			start++;
-			end--;
-		}
-		
-	}
-	
-	static void reverseArrayoOnRange(int arr[],int pos1, int pos2) {
-		int start = pos1;
-		int end = pos2;
-		if(start>end) {
-			System.out.println("Position cannot be Greater than Position 2");
-		}
-		if(end>arr.length-1) {
-			System.out.println("Index out of Bound Brother.");
-		}
-		while(start<end) {
-			swapArray(arr, start, end);
-			start++;
-			end--;
-		}
-		
-	}
-	
-	static Integer maxVal(int arr[]) {
-		int maxVal=Integer.MIN_VALUE;
-		for(int i = 0; i<arr.length;i++) {
-			if(arr[i]>maxVal) {
-				maxVal=arr[i];
+		for(int i = 0;i<nums.length;i++) {
+			for(int j=0;j<nums[i].length;j++) {
+				System.out.print(nums[i][j]+" ");
 			}
+			System.out.println();
 		}
-		return maxVal;
 		
+
 	}
+
+
 	
 }
