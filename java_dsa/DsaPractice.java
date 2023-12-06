0e28dcc6448d410292029e9b45db74f13e9d2c65
Kishore-r-77 <kishorework77@gmail.com>
2023-12-06 16:56:46 +0530

    practiced FirstAndLastPosition problem in java

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,28 +1,49 @@
 package java_dsa;
 
+import java.util.Arrays;
+
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		char[] alphabets = { 'a', 'e', 'f', 'j' };
-
-		System.out.println(nextGreatestLetter(alphabets, 'a'));
+		int[] nums= {5,7,7,8,8,10};
+		System.out.println(Arrays.toString(searchRange(nums, 8)));
 
 	}
-
-	static char nextGreatestLetter(char[] letters, char target) {
+	
+	static int[] searchRange(int[] arr,int target) {
+		int[] ans= {-1,-1};
+		ans[0]=search(arr, target, true);
+		ans[1]=search(arr, target, false);
+		return ans;
+	}
+	
+	
+	
+	static int search(int[] arr,int target,boolean isFirstIndex) {
+		int ans = -1;
 		int start = 0;
-		int end = letters.length - 1;
-
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			if (target < letters[mid]) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
+		int end = arr.length-1;
+		while(start<=end) {
+			int mid = start+(end-start)/2;
+			if(target<arr[mid]) {
+				end=mid-1;
+			}
+			else if(target>arr[mid]){
+				start=mid+1;
+			}
+			else {
+				ans=mid;
+				if(isFirstIndex) {
+					end=mid-1;
+				}
+				else {
+					start=mid+1;
+				}
 			}
 		}
-
-		return letters[start % letters.length];
+		return ans;
 	}
+
+	
 
 }
