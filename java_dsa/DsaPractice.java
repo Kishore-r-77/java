cd579b9ae0ee396458abcd0029e4e24cc5083cd5
Kishore-r-77 <kishorework77@gmail.com>
2023-12-06 15:31:14 +0530

    practiced nextGreatestLetter problem

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -3,46 +3,26 @@ package java_dsa;
 public class DsaPractice {
 	public static void main(String[] args) {
 
-	
-		int numbers[]= {1,3,4,5,7};
-		System.out.println(floor(numbers,6));
-		
+		char[] alphabets = { 'a', 'e', 'f', 'j' };
+
+		System.out.println(nextGreatestLetter(alphabets, 'a'));
 
 	}
-	
-	static int floor(int arr[],int target) {
+
+	static char nextGreatestLetter(char[] letters, char target) {
 		int start = 0;
-		int end = arr.length-1;
-		boolean isAsc=arr[start]<arr[end];
-		
-		
-		while(start<=end) {
-			int mid = start+(end-start)/2;
-			
-			
-			if(target==arr[mid]) {
-				return mid;
+		int end = letters.length - 1;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (target < letters[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
 			}
-			if(isAsc) {
-				if(target>arr[mid]) {
-					start=mid+1;
-				}
-				else {
-					end=mid-1;
-				}
-			}
-			else {
-				if(target>arr[mid]) {
-					end=mid-1;
-				}
-				else {
-					start=mid+1;
-				}
-			}
-			
 		}
-		return end;
+
+		return letters[start % letters.length];
 	}
-	
 
 }
