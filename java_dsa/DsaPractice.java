3d298774681e3135d0a60a184a5ddf52c886aaca
Kishore-r-77 <kishorework77@gmail.com>
2023-12-07 20:27:47 +0530

    tried working on infinite array problem. it works partially still has some issues

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,49 +1,38 @@
 package java_dsa;
 
-import java.util.Arrays;
-
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		int[] nums= {5,7,7,8,8,10};
-		System.out.println(Arrays.toString(searchRange(nums, 8)));
+		int[] nums = { 1, 2, 3, 4, 5, 6, 7 ,8};
+		
+		System.out.println(dynamicSearch(nums, 7));
 
 	}
-	
-	static int[] searchRange(int[] arr,int target) {
-		int[] ans= {-1,-1};
-		ans[0]=search(arr, target, true);
-		ans[1]=search(arr, target, false);
-		return ans;
-	}
-	
-	
-	
-	static int search(int[] arr,int target,boolean isFirstIndex) {
-		int ans = -1;
+
+	static int dynamicSearch(int[] arr, int target) {
 		int start = 0;
-		int end = arr.length-1;
-		while(start<=end) {
-			int mid = start+(end-start)/2;
-			if(target<arr[mid]) {
-				end=mid-1;
-			}
-			else if(target>arr[mid]){
-				start=mid+1;
-			}
-			else {
-				ans=mid;
-				if(isFirstIndex) {
-					end=mid-1;
-				}
-				else {
-					start=mid+1;
-				}
+		int end = 1;
+
+		if (target > arr[end]) {
+			int temp = end + 1;
+			end = end + (end - start + 1) * 2;
+			start = temp;
+		}
+		return binarySearch(arr, target, start, end);
+	}
+
+	static int binarySearch(int[] arr, int target, int start, int end) {
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (target < arr[mid]) {
+				end = mid - 1;
+			} else if (target > arr[mid]) {
+				start = mid + 1;
+			} else {
+				return mid;
 			}
 		}
-		return ans;
+		return -1;
 	}
-
-	
 
 }
