5f4fa23aeca793fb754510750db2b1ef7732be30
Kishore-r-77 <kishorework77@gmail.com>
2023-12-07 20:33:33 +0530

    worked on finding the peak element

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -3,36 +3,25 @@ package java_dsa;
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		int[] nums = { 1, 2, 3, 4, 5, 6, 7 ,8};
-		
-		System.out.println(dynamicSearch(nums, 7));
+		int[] nums = { 22, 33, 44, 10, 8, 7, 2 };
+
+		System.out.println(findPeakElement(nums));
 
 	}
 
-	static int dynamicSearch(int[] arr, int target) {
+	static int findPeakElement(int arr[]) {
 		int start = 0;
-		int end = 1;
+		int end = arr.length - 1;
 
-		if (target > arr[end]) {
-			int temp = end + 1;
-			end = end + (end - start + 1) * 2;
-			start = temp;
-		}
-		return binarySearch(arr, target, start, end);
-	}
-
-	static int binarySearch(int[] arr, int target, int start, int end) {
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			if (target < arr[mid]) {
-				end = mid - 1;
-			} else if (target > arr[mid]) {
-				start = mid + 1;
+		while (start < end) {
+			int mid = start + (end - 1) / 2;
+			if (arr[mid] > arr[mid + 1]) {
+				end = mid;
 			} else {
-				return mid;
+				start = mid + 1;
 			}
 		}
-		return -1;
+		return start;
 	}
 
 }
