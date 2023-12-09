bfebefa0ee819d78f35e84c0f2cbeb46535c3a2a
Kishore-r-77 <kishorework77@gmail.com>
2023-12-09 18:10:11 +0530

    practiced twosum problem in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -6,24 +6,22 @@ class Practice {
 
 	public static void main(String[] args) {
 
-		int[] nums = { 1, 7, 8, 9, 22 };
-
-		System.out.println(Arrays.toString(twoSum(nums, 17)));
+		int nums[]= {1,8,9,22};
+		System.out.println(Arrays.toString(twoSum(nums, 300)));
 
 	}
-
-	static int[] twoSum(int[] arr, int target) {
-		int[] ans = new int[2];
-		for (int i = 0; i <= arr.length - 1; i++) {
-			for (int j = 1; j <= arr.length - 1; j++) {
-				if (arr[i] + arr[j] == target) {
-					ans[0] = i;
-					ans[1] = j;
-					return ans;
+		
+	static int[] twoSum(int[] arr,int target) {
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=1;j<arr.length;j++) {
+				if(arr[i]+arr[j]==target) {
+					return new int[] {i,j};
 				}
 			}
 		}
-		return ans;
+		return new int[] {};
 	}
+	
 	
 }
