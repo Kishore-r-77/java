91413b34ad2b478b345df5deef156d26ffe3e471
Kishore-r-77 <kishorework77@gmail.com>
2023-12-14 21:07:24 +0530

    practiced missing numbers in java

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,18 +1,19 @@
 package java_dsa;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		int[] nums = { 3,5,2,1,4 };
-		cyclicSort(nums);
-		System.out.println(Arrays.toString(nums));
+		int[] nums = { 4,3,2,7,8,2,3,1 };
+		findingTheMissingNumbers(nums);
+		System.out.println(findingTheMissingNumbers(nums));
 		
 
 	}
 
-	static void cyclicSort(int[] arr) {	
+	static List<Integer> findingTheMissingNumbers(int[] arr) {	
 		
 		int i=0;
 		while(i<arr.length) {
@@ -24,6 +25,13 @@ public class DsaPractice {
 				i++;
 			}
 		}
+		List<Integer> ans = new ArrayList<>();
+		for(int index=0;index<arr.length;index++) {
+			if(arr[index]!=index+1) {
+				ans.add(index+1);
+			}
+		}
+		return ans;
 				
 	}
 	
