660ba084fe4c4a693f5fad36f15d35133201fecf
Kishore-r-77 <kishorework77@gmail.com>
2023-12-15 15:56:04 +0530

    checked String assigning

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,27 +1,17 @@
 package practice;
 
-import java.util.Arrays;
-
 class Practice {
 
 	public static void main(String[] args) {
 
-		int nums[]= {1,8,9,22};
-		System.out.println(Arrays.toString(twoSum(nums, 300)));
-
+			String a="Kishore";
+			String b=a;
+			
+			b="Kish";
+			System.out.println(a);
 	}
 		
-	static int[] twoSum(int[] arr,int target) {
-		
-		for(int i=0;i<arr.length;i++) {
-			for(int j=1;j<arr.length;j++) {
-				if(arr[i]+arr[j]==target) {
-					return new int[] {i,j};
-				}
-			}
-		}
-		return new int[] {};
-	}
+	
 	
 	
 }
