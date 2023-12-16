610b9ddbc875c872b17cfc9d8000a79453beb4c5
Kishore-r-77 <kishorework77@gmail.com>
2023-12-16 15:42:37 +0530

    practiced pattern4 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -10,8 +10,8 @@ class Practice {
 	
 	static void pattern3(int n) {
 		for(int row=1;row<=n;row++) {
-			for(int col=1;col<=n-row+1;col++) {
-				System.out.print("* ");
+			for(int col=1;col<=row;col++) {
+				System.out.print(col+" ");
 			}
 			System.out.println();
 		}
