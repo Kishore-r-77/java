13ab2dbe8059019e2e058b2f62582ffa4e9c5963
Kishore-r-77 <kishorework77@gmail.com>
2023-12-31 21:15:04 +0530

    practiced Multithreading in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,9 +1,32 @@
 package practice;
 
+class Person extends Thread {
+	String name;
+
+	Person(String name) {
+		this.name = name;
+	}
+
+	public void run() {
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i + " hello " + this.name);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
 class Practice {
 
 	public static void main(String[] args) {
-
+		Person kishore = new Person("Kishore");
+		Person ajay = new Person("Ajay");
+		kishore.start();
+		ajay.start();
 	}
 
 }
