d57302d42c04fe526b50497d00172b2bb87b66a9
Kishore-r-77 <kishorework77@gmail.com>
2023-12-31 12:45:09 +0530

    practiced order agnostic binary search

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,48 +1,38 @@
 package java_dsa;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DsaPractice {
 	public static void main(String[] args) {
 
-		int[] nums = { 4,3,2,7,8,2,3,1 };
-		findingTheMissingNumbers(nums);
-		System.out.println(findingTheMissingNumbers(nums));
-		
+		int[] nums = new int[] { 5, 4, 3, 2, 1 };
+		System.out.println(binarySearch(nums, 4));
 
 	}
 
-	static List<Integer> findingTheMissingNumbers(int[] arr) {	
-		
-		int i=0;
-		while(i<arr.length) {
-			int correct=arr[i]-1;
-			if(arr[i]!=arr[correct]) {
-				swapArray(arr, i, correct);
+	static int binarySearch(int[] arr, int target) {
+		int start = 0;
+		int end = arr.length - 1;
+		boolean isAsc = arr[start] < arr[target];
+		while (start <= end) {
+
+			int mid = start + (end - start) / 2;
+			if (target == arr[mid]) {
+				return mid;
 			}
-			else {
-				i++;
+			if (isAsc) {
+				if (target > arr[mid]) {
+					start = start + 1;
+				} else {
+					end = mid - 1;
+				}
+			} else {
+				if (target > arr[mid]) {
+					end = mid - 1;
+				} else {
+					start = start + 1;
+				}
 			}
 		}
-		List<Integer> ans = new ArrayList<>();
-		for(int index=0;index<arr.length;index++) {
-			if(arr[index]!=index+1) {
-				ans.add(index+1);
-			}
-		}
-		return ans;
-				
+		return -1;
 	}
-	
-	static void swapArray(int arr[],int index1, int index2) {
-		if (index1 >= arr.length || index2 >= arr.length) {
-		    System.out.println("Then index is out of bound");
-		}
-		int temp=arr[index1];
-		arr[index1]=arr[index2];
-		arr[index2]=temp;
-	}
-	
-	
+
 }
--- practice/Practice.java
+++ practice/Practice.java
@@ -3,21 +3,7 @@ package practice;
 class Practice {
 
 	public static void main(String[] args) {
-		
-		display(1);
 
 	}
-	
-	static void display(int count) {
-		if(count==5) {
-			return;
-		}
-		
-		System.out.println("Hello Kishore");
-		display(++count);
-		
-		
-	}
 
-	
 }
