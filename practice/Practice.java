bc3a8d852d00f20a0f411f5fe595093bece69a8a
Kishore-r-77 <kishorework77@gmail.com>
2024-01-11 09:04:52 +0530

    practiced pattern1

--- practice/Practice.java
+++ practice/Practice.java
@@ -12,7 +12,7 @@ class Practice {
 	
 	static void pattern2(int n) {
 		for(int row=1;row<=n;row++) {
-			for(int col=1;col<=row;col++) {
+			for(int col=1;col<=n;col++) {
 				System.out.print("* ");
 			}
 			System.out.println();
