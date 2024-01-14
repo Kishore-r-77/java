bf1027efabf7ecd00fbc98c17dc964413718bb03
Kishore-r-77 <kishorework77@gmail.com>
2024-01-14 18:47:46 +0530

    practiced triangle 2 using recursion

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,19 +3,19 @@ package practice;
 class Practice {
 
 	public static void main(String[] args) {
-		triangle1(5, 0);
+		triangle2(5, 0);
 	}
 
-	static void triangle1(int r, int c) {
+	static void triangle2(int r, int c) {
 		if (r == 0) {
 			return;
 		}
 		if (r > c) {
+			triangle2(r, c + 1);
 			System.out.print("*");
-			triangle1(r, c + 1);
 		} else {
+			triangle2(r - 1, 0);
 			System.out.println();
-			triangle1(r - 1, 0);
 		}
 	}
 
