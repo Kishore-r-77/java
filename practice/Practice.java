d9cada20e1bf038ac125048227edd1bc2731e2e5
Kishore-r-77 <kishorework77@gmail.com>
2024-01-17 05:50:53 +0530

    practiced reversing of number in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,30 +1,21 @@
 package practice;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 class Practice {
 
 	public static void main(String[] args) {
-		int[] arr = { 4, 2, 3, 1 };
-		selectionSortRecursion(arr, arr.length - 1, 0, 0);
-		System.out.println(Arrays.toString(arr));
+		int num = 123456;
+		int ans = 0;
+
+		while (num > 0) {
+			int rem = num % 10;
+			num = num / 10;
+			ans = ans * 10 + rem;
+		}
+
+		System.out.println(ans);
 	}
 
-	static void selectionSortRecursion(int[] arr, int r, int c, int max) {
-		if (r == 0) {
-			return;
-		}
-		if (c < r) {
-			if (arr[c] > arr[max]) {
-				selectionSortRecursion(arr, r, c + 1, c);
-			} else {
-				selectionSortRecursion(arr, r, c + 1, max);
-			}
-		} else {
-			int temp = arr[max];
-			arr[max] = arr[r - 1];
-			arr[r - 1] = temp;
-			selectionSortRecursion(arr, r - 1, 0, 0);
-		}
-	}
 }
