c0f4de819f37a5f0bff3acedf6b1b2cf2ec6e475
Kishore-r-77 <kishorework77@gmail.com>
2024-01-21 18:16:40 +0530

    practiced stream and method reference

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,21 +1,12 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 
 class Practice {
 
 	public static void main(String[] args) {
-		int num = 123456;
-		int ans = 0;
-
-		while (num > 0) {
-			int rem = num % 10;
-			num = num / 10;
-			ans = ans * 10 + rem;
-		}
-
-		System.out.println(ans);
+		List<Integer> nums = List.of(1, 2, 3, 4, 7);
+		nums.stream().forEach(System.out::println);
 	}
 
 }
