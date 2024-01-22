464c765d132a89ec169d1e53b8379d9d16f73dcf
Kishore-r-77 <kishorework77@gmail.com>
2024-01-22 10:45:33 +0530

    practiced build array from permutations

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,8 +1,20 @@
 package java_dsa;
 
+import java.util.Arrays;
+
 public class DsaPractice {
 	public static void main(String[] args) {
+		int[] nums = { 0, 2, 1, 5, 3, 4 };
+		System.out.println(Arrays.toString(buildArray(nums)));
+	}
 
+	static public int[] buildArray(int[] nums) {
+
+		int[] result = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			result[i] = nums[nums[i]];
+		}
+		return result;
 	}
 
 }
--- practice/Practice.java
+++ practice/Practice.java
@@ -1,12 +1,9 @@
 package practice;
 
-import java.util.List;
-
 class Practice {
 
 	public static void main(String[] args) {
-		List<Integer> nums = List.of(1, 2, 3, 4, 7);
-		nums.stream().forEach(System.out::println);
+
 	}
 
 }
