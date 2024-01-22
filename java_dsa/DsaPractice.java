83259126876811d004994224f58df2a504cec0e5
Kishore-r-77 <kishorework77@gmail.com>
2024-01-22 13:21:53 +0530

    practiced running sum in array questions

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -4,16 +4,21 @@ import java.util.Arrays;
 
 public class DsaPractice {
 	public static void main(String[] args) {
-		int[] nums = { 1, 3, 1 };
-		System.out.println(Arrays.toString(concatenationOfArray(nums)));
+		int[] nums = { 1, 2, 3, 4 };
+		System.out.println(Arrays.toString(runningSum(nums)));
+
 	}
 
-	public static int[] concatenationOfArray(int[] nums) {
-		int[] newArray = new int[nums.length * 2];
-		int n = nums.length;
-		System.arraycopy(nums, 0, newArray, 0, n);
-		System.arraycopy(nums, 0, newArray, n, n);
+	// output=[1,3,6,10]
+	public static int[] runningSum(int[] nums) {
+		int[] newArray = new int[nums.length];
+		int runningSum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			runningSum += nums[i];
+			newArray[i] = runningSum;
+		}
 		return newArray;
+
 	}
 
 }
