9c21cbda4eb635dbb0a62de18180af73a20f66db
Kishore-r-77 <kishorework77@gmail.com>
2024-01-22 13:52:36 +0530

    practiced max wealth in array questions

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -4,21 +4,27 @@ import java.util.Arrays;
 
 public class DsaPractice {
 	public static void main(String[] args) {
-		int[] nums = { 1, 2, 3, 4 };
-		System.out.println(Arrays.toString(runningSum(nums)));
+		int[][] nums = {
+				{ 1, 5 },
+				{ 7, 3 },
+				{ 3, 5 }
+		};
+		System.out.println(maxWealth(nums));
 
 	}
 
-	// output=[1,3,6,10]
-	public static int[] runningSum(int[] nums) {
-		int[] newArray = new int[nums.length];
-		int runningSum = 0;
-		for (int i = 0; i < nums.length; i++) {
-			runningSum += nums[i];
-			newArray[i] = runningSum;
+	public static int maxWealth(int[][] arr) {
+		int total = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			for (int j = 0; j < arr[i].length; j++) {
+				sum += arr[i][j];
+				if (total < sum) {
+					total = sum;
+				}
+			}
 		}
-		return newArray;
-
+		return total;
 	}
 
 }
