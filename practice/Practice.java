14023546a76d79c83deb8a5c527c4ba5c8fadc5a
Kishore-r-77 <kishorework77@gmail.com>
2024-01-24 11:52:21 +0530

    practiced abstract classes

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,9 +1,44 @@
 package practice;
 
+abstract class Animal {
+	String name;
+	String category;
+
+	abstract void makeNoise();
+
+	void display() {
+		System.out.println("I'm animal");
+	}
+}
+
+class Cat extends Animal {
+
+	@Override
+	void makeNoise() {
+		System.out.println("Meow Meow");
+	}
+
+	void display() {
+		System.out.println("I'm Cat");
+	}
+
+	String getName() {
+		return name;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+}
+
 class Practice {
 
 	public static void main(String[] args) {
-
+		Cat cat = new Cat();
+		cat.setName("Kitty");
+		cat.makeNoise();
+		System.out.println(cat.name);
 	}
 
 }
