70f08e44db18f9dd9693cfae7fd2b30c0fe07ba8
Kishore-r-77 <kishorework77@gmail.com>
2024-01-26 14:40:24 +0530

    practiced good pairs sum in an enhanced way

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,5 +1,8 @@
 package java_dsa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DsaPractice {
 	public static void main(String[] args) {
 		int[] nums = { 1, 2, 3, 1, 1, 3 };
@@ -7,14 +10,16 @@ public class DsaPractice {
 	}
 
 	static int goodPairs(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
 		int count = 0;
-		for (int i = 0; i < nums.length - 1; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (nums[i] == nums[j]) {
-					count += 1;
-				}
+
+		for (int num : nums) {
+			if (map.containsKey(num)) {
+				count += map.get(num);
 			}
+			map.put(num, map.getOrDefault(num, 0) + 1);
 		}
+
 		return count;
 	}
 
