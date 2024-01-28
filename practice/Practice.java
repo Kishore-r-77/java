2edd0af629024b9c6911c3d68d88fa14bee29d3a
Kishore-r-77 <kishorework77@gmail.com>
2024-01-28 21:06:50 +0530

    practiced initialization array.aslist and list.of in collection

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,11 +1,20 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class Practice {
 
 	public static void main(String[] args) {
+		List<Integer> listArray = new ArrayList<>(List.of(1, 2, 3));
+		List<Integer> listArray2 = new ArrayList<>(Arrays.asList(1, 2, 3, null));
+		listArray.add(4);
+		listArray2.add(4);
+		System.out.println(listArray);
+		System.out.println(listArray2);
 		Map<String, String> person = new HashMap<>();
 		person.put("name", "Kishore");
 		person.put("age", "27");
