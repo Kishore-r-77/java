8065db4ad58d6798ce32b57c180329a5cc00533a
Kishore-r-77 <kishorework77@gmail.com>
2024-01-31 05:27:31 +0530

    create target Array Problem

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,26 +1,39 @@
 package java_dsa;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DsaPractice {
+	// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+	// Output: [0,4,1,3,2]
+	// Explanation:
+	// nums index target
+	// 0 0 [0]
+	// 1 1 [0,1]
+	// 2 2 [0,1,2]
+	// 3 2 [0,1,3,2]
+	// 4 1 [0,4,1,3,2]
 	public static void main(String[] args) {
-		int[] nums = { 1, 2, 3, 1, 1, 3 };
-		System.out.println(goodPairs(nums));
+		int[] nums = { 0, 1, 2, 3, 4 };
+		int[] index = { 0, 1, 2, 2, 1 };
+		System.out.println(Arrays.toString(createTargetArray(nums, index)));
 	}
 
-	static int goodPairs(int[] nums) {
-		Map<Integer, Integer> map = new HashMap<>();
-		int count = 0;
+	static int[] createTargetArray(int[] nums, int[] index) {
+		int[] target = new int[nums.length];
 
-		for (int num : nums) {
-			if (map.containsKey(num)) {
-				count += map.get(num);
-			}
-			map.put(num, map.getOrDefault(num, 0) + 1);
+		List<Integer> newArray = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			newArray.add(index[i], nums[i]);
 		}
 
-		return count;
+		for (int i = 0; i < nums.length; i++) {
+			target[i] = newArray.get(i);
+		}
+
+		return target;
 	}
 
 }
--- practice/Practice.java
+++ practice/Practice.java
@@ -10,16 +10,11 @@ class Practice {
 
 	public static void main(String[] args) {
 		List<Integer> listArray = new ArrayList<>(List.of(1, 2, 3));
-		List<Integer> listArray2 = new ArrayList<>(Arrays.asList(1, 2, 3, null));
 		listArray.add(4);
-		listArray2.add(4);
+
+		listArray.add(0, 7);
+
 		System.out.println(listArray);
-		System.out.println(listArray2);
-		Map<String, String> person = new HashMap<>();
-		person.put("name", "Kishore");
-		person.put("age", "27");
-		person.put("role", "FullStack Developer");
-		person.put("company", "Netflix");
 
 	}
 
