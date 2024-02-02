d7fbfe34d7c49a679a9352eb80463fd49e311fde
Kishore-r-77 <kishorework77@gmail.com>
2024-02-02 15:30:44 +0530

    practiced largest altitude problem

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,45 +1,24 @@
 package java_dsa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class DsaPractice {
-	// Input: items =
-	// [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]],
-	// ruleKey = "color", ruleValue = "silver"
-	// Output: 1
-	// Explanation: There is only one item matching the given rule, which is
-	// ["computer","silver","lenovo"].
+
 	public static void main(String[] args) {
-		List<String> item1 = Arrays.asList("phone", "blue", "pixel");
-		List<String> item2 = Arrays.asList("computer", "silver", "phone");
-		List<String> item3 = Arrays.asList("phone", "gold", "iphone");
+		// Input: gain = [-5,1,5,0,-7]
+		// Output: 1
+		// Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 
-		List<List<String>> listItems = new ArrayList<>(Arrays.asList(item1, item2, item3));
-		System.out.println(countMatches(listItems, "type", "phone"));
+		int[] gain = { -5, 1, 5, 0, -7 };
+		System.out.println(largestAltitude(gain));
 	}
 
-	static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-		int count = 0;
-
-		int ruleIndex = -1;
-		if (ruleKey.equals("type")) {
-			ruleIndex = 0;
-		} else if (ruleKey.equals("color")) {
-			ruleIndex = 1;
-		} else if (ruleKey.equals("name")) {
-			ruleIndex = 2;
+	static int largestAltitude(int[] gain) {
+		int res = 0;
+		int alt = 0;
+		for (int climb : gain) {
+			alt += climb;
+			res = Math.max(res, alt);
 		}
-
-		if (ruleIndex != -1) {
-			for (List<String> item : items) {
-				if (item.get(ruleIndex).equals(ruleValue)) {
-					count++;
-				}
-			}
-		}
-
-		return count;
+		return res;
 	}
+
 }
