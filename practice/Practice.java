6a6ba924624bc64b01c493667a2eb9917034b9b0
Kishore-r-77 <kishorework77@gmail.com>
2024-02-04 16:49:59 +0530

    practiced isAnagram program

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,26 +1,28 @@
 package practice;
 
+import java.util.Arrays;
+
 class Practice {
 
 	public static void main(String[] args) {
-		String name = "abcba";
-		System.out.println(isPalindrome(name));
+		String name1 = "Listen";
+		String name2 = "Silent";
+
+		System.out.println(isAnagram(name1, name2));
 
 	}
 
-	static Boolean isPalindrome(String name) {
-		char[] charArray = name.toCharArray();
-		String str = name.toLowerCase();
+	static Boolean isAnagram(String name1, String name2) {
+		char[] array1 = name1.toLowerCase().toCharArray();
+		char[] array2 = name2.toLowerCase().toCharArray();
+		Arrays.sort(array1);
+		Arrays.sort(array2);
+		System.out.println(array1);
+		System.out.println(array1);
 
-		for (int i = 0; i < charArray.length / 2; i++) {
-			char start = str.charAt(i);
-			char end = str.charAt(charArray.length - 1 - i);
-			if (start == end) {
-				return true;
-			}
+		System.out.println(array1);
+		return Arrays.equals(array1, array2);
 
-		}
-		return false;
 	}
 
 }
