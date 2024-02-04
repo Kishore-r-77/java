fc1400512472ebbe2f9c978b508d5f67861c1545
Kishore-r-77 <kishorework77@gmail.com>
2024-02-04 15:57:35 +0530

    practiced isPalindrome program

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,11 +3,24 @@ package practice;
 class Practice {
 
 	public static void main(String[] args) {
-		String name1 = "Kishore";
-		String name2 = "Kishore";
+		String name = "abcba";
+		System.out.println(isPalindrome(name));
 
-		System.out.println(name1 == name2);
+	}
 
+	static Boolean isPalindrome(String name) {
+		char[] charArray = name.toCharArray();
+		String str = name.toLowerCase();
+
+		for (int i = 0; i < charArray.length / 2; i++) {
+			char start = str.charAt(i);
+			char end = str.charAt(charArray.length - 1 - i);
+			if (start == end) {
+				return true;
+			}
+
+		}
+		return false;
 	}
 
 }
