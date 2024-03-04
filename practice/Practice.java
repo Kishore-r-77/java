e1c9179fa6a7ffe77822b0d3111c53518fe97575
Kishore-r-77 <kishorework77@gmail.com>
2024-03-04 14:37:10 +0530

    practiced flipping image problem in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,30 +1,47 @@
 package practice;
 
-interface Kishore {
-	default void display() {
-		System.out.println("I'm a Kishore");
-	}
-}
-
-interface Ash {
-	default void display() {
-		System.out.println("I'm Ash");
-	}
-}
-
-class Son implements Kishore, Ash {
-	@Override
-	public void display() {
-
-		Ash.super.display();
-	}
-
-}
+import java.util.Arrays;
+import java.util.List;
 
 class Practice {
 	public static void main(String[] args) {
-		Son son = new Son();
-		son.display();
+		// Output: [[1,0,0],[0,1,0],[1,1,1]]
+
+		int[][] image = {
+				{ 1 },
+
+		};
+
+		System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
+
 	}
 
+	static public int[][] flipAndInvertImage(int[][] image) {
+		int start = 0;
+		int end = image.length - 1;
+		for (int[] innerArray : image) {
+			while (start <= end) {
+				int temp = innerArray[start];
+				innerArray[start++] = innerArray[end] ^ 1;
+				innerArray[end--] = temp ^ 1;
+			}
+		}
+
+		return image;
+	}
+
+	static int[] swapArray(int[] arr, int start, int end) {
+
+		int[] result = new int[arr.length];
+		if (start <= end) {
+
+			while (start <= end) {
+				result[start] = arr[end];
+				result[end] = arr[start];
+				start++;
+				end--;
+			}
+		}
+		return result;
+	}
 }
