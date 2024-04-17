9d60b529c5f47024f49d98525ed9b2ae1545b58e
Kishore <kishorework77@gmail.com>
2024-04-17 05:55:21 +0530

    updated

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,24 +1,48 @@
 package java_dsa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class DsaPractice {
 
 	public static void main(String[] args) {
-		// Input: gain = [-5,1,5,0,-7]
-		// Output: 1
-		// Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+		// Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+		// Output: [15]
+		// Explanation: 15 is the only lucky number since it is the minimum in its row
+		// and the maximum in its column.
 
-		int[] gain = { -5, 1, 5, 0, -7 };
-		System.out.println(largestAltitude(gain));
+		int[][] nums = {
+				{ 3, 7, 8 },
+				{ 9, 11, 13 },
+				{ 15, 16, 17 }
+		};
+		System.out.println(luckyNumbers(nums));
+
 	}
 
-	static int largestAltitude(int[] gain) {
-		int res = 0;
-		int alt = 0;
-		for (int climb : gain) {
-			alt += climb;
-			res = Math.max(res, alt);
+	static public List<Integer> luckyNumbers(int[][] matrix) {
+
+		List<Integer> output = new ArrayList<>();
+		int minRow = 0;
+		int maxCol = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			int index = 0;
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (minRow > matrix[i][j]) {
+					minRow = matrix[i][j];
+					index = i;
+				}
+				if (maxCol < matrix[j][i]) {
+					maxCol = matrix[j][index];
+
+				}
+			}
 		}
-		return res;
+
+		output.add(maxCol);
+		return output;
 	}
 
 }
--- practice/Practice.java
+++ practice/Practice.java
@@ -1,42 +1,9 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 class Practice {
-	public static void main(String[] args) {
-		// Output: 25
-
-		int[][] mat = {
-				{ 1, 1, 1, 1 },
-				{ 1, 1, 1, 1 },
-				{ 1, 1, 1, 1 },
-				{ 1, 1, 1, 1 },
-
-		};
-
-		System.out.println(diagonalSum(mat));
-
-	}
-
-	static public int diagonalSum(int[][] mat) {
-		int n = mat.length;
-		int sum = 0;
-
-		for (int i = 0; i < n; i++) {
-			sum += mat[i][i]; // Sum of primary diagonal elements
-			sum += mat[i][n - i - 1]; // Sum of secondary diagonal elements
-		}
-
-		// If the matrix size is odd, we've counted the middle element twice
-		if (n % 2 == 1) {
-			sum -= mat[n / 2][n / 2];
-		}
-
-		return sum;
+	public static void main(String[] args){
+		
+		System.out.println("Hello World");
 	}
 
 }
