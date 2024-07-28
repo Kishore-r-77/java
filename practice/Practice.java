44c600c397e30f1a9d4edcf2301720502add76bf
Kishore <kishorework77@gmail.com>
2024-07-28 15:23:28 +0530

    practiced recursio in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,25 +1,16 @@
 package practice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Practice {
 	public static void main(String[] args) {
-		int[] nums = { 2, 1, 1, 2, 3, 5, 1, 2, 4 };
-
-		System.out.println(recurring(nums));
+		recursion(0);
 	}
 
-	static int recurring(int[] nums) {
-		Map<Integer, Integer> hashTable = new HashMap<>();
-		for (int i = 0; i < nums.length; i++) {
-			if (hashTable.containsKey(nums[i])) {
-				return nums[i];
-			} else {
-				hashTable.put(nums[i], i);
-			}
+	static public void recursion(int n) {
+		if (n == 5) {
+			return;
 		}
-		return -1;
+		System.out.println("The number is " + n);
+		recursion(n++);
 	}
 
 }
