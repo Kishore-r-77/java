aad60bd13b2000338ce2a67d82bc6a0075612847
Kishore <kishorework77@gmail.com>
2024-08-04 11:57:23 +0530

    practiced insertion sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -6,15 +6,14 @@ class Practice {
 	public static void main(String[] args) {
 		int[] nums = { 5, 4, 3, 2, 1 };
 
-		bubbleSort(nums);
+		insertionSort(nums);
 		System.out.println(Arrays.toString(nums));
 
 	}
 
-	static void bubbleSort(int[] arr) {
-
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = 1; j < arr.length - i; j++) {
+	static void insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = i; j > 0; j--) {
 				if (arr[j] < arr[j - 1]) {
 					swap(arr, j, j - 1);
 				}
