fafea3b86436449c4087802d837706d672eee7d6
Kishore <kishorework77@gmail.com>
2024-08-21 20:46:23 +0530

    practiced insertion of element in position of an array in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -2,39 +2,70 @@ package practice;
 
 import java.util.Arrays;
 
+class CustomArrays {
+	int data[];
+
+	int size;
+
+	int capacity;
+
+	int DEFAULT_CAPACITY = 10;
+
+	CustomArrays() {
+		this.capacity = DEFAULT_CAPACITY;
+		this.data = new int[capacity];
+	}
+
+	void expandArray() {
+		capacity *= 2;
+		data = Arrays.copyOf(data, capacity);
+	}
+
+	void shrinkArray() {
+		capacity /= 2;
+		data = Arrays.copyOf(data, capacity);
+	}
+
+	void display() {
+		for (int i = 0; i < data.length; i++) {
+			System.out.print(data[i] + " ");
+		}
+		System.out.println();
+	}
+
+	void add(int val) {
+		if (size == capacity) {
+			expandArray();
+		}
+		data[size++] = val;
+	}
+
+	// [3,2,5,2,7,9,8]
+	void insertAtPos(int val, int pos) {
+		if (size == capacity) {
+			expandArray();
+		}
+		for (int i = size - 1; i >= pos; i--) {
+			data[i + 1] = data[i];
+		}
+		data[pos] = val;
+		size++;
+	}
+
+	void deleteAtEnd() {
+
+	}
+}
+
 class Practice {
 	public static void main(String[] args) {
-		int[] nums = { 5, 4, 3, 2, 1 };
-
-		selectionSort(nums);
-		System.out.println(Arrays.toString(nums));
-
-	}
-
-	static void selectionSort(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-
-			int maxIndex = getMaxIndex(arr, 0, arr.length - i - 1);
-			swap(arr, maxIndex, arr.length - i - 1);
-		}
-	}
-
-	static int getMaxIndex(int[] arr, int start, int end) {
-		int maxIndex = start;
-		for (int i = 0; i <= end; i++) {
-			if (arr[maxIndex] < arr[i]) {
-				maxIndex = i;
-			}
-		}
-
-		return maxIndex;
-
-	}
-
-	static void swap(int[] arr, int start, int end) {
-		int temp = arr[start];
-		arr[start] = arr[end];
-		arr[end] = temp;
+		CustomArrays customArrays = new CustomArrays();
+		customArrays.add(1);
+		customArrays.add(2);
+		customArrays.add(3);
+		customArrays.add(4);
+		customArrays.insertAtPos(7, 1);
+		customArrays.display();
 	}
 
 }
