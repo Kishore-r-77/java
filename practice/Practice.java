e0213ebce1ff723c08e35ffb2ad36f315f0f83ec
Kishore <kishorework77@gmail.com>
2024-08-26 12:57:07 +0530

    practiced selection sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -6,21 +6,27 @@ class Practice {
 	public static void main(String[] args) {
 
 		int[] nums = { 5, 4, 3, 2, 1 };
-		insertion(nums);
+		selectionSort(nums);
 		System.out.println(Arrays.toString(nums));
 	}
 
-	static void insertion(int[] arr) {
+	static void selectionSort(int[] arr) {
 
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = i + 1; j > 0; j--) {
-				if (arr[j] < arr[j - 1]) {
-					swap(arr, j, j - 1);
-				} else {
-					break;
-				}
+		for (int i = 0; i < arr.length; i++) {
+			int last = arr.length - i - 1;
+			int maxIndex = getMaxIndex(arr, 0, last);
+			swap(arr, maxIndex, last);
+		}
+	}
+
+	static int getMaxIndex(int arr[], int start, int end) {
+		int maxIndex = start;
+		for (int i = start; i <= end; i++) {
+			if (arr[maxIndex] < arr[i]) {
+				maxIndex = i;
 			}
 		}
+		return maxIndex;
 	}
 
 	static void swap(int[] arr, int start, int end) {
