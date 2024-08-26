0769ddc62859ecc648d500a706feaae17b5e14fd
Kishore <kishorework77@gmail.com>
2024-08-26 12:25:25 +0530

    practiced bubble sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,78 +1,35 @@
 package practice;
 
-class CircularLinkedList {
-
-	Node head;
-	Node tail;
-
-	private class Node {
-		int data;
-		Node next;
-
-		Node(int val) {
-			this.data = val;
-			this.next = null;
-		}
-	}
-
-	void insert(int val) {
-		Node newNode = new Node(val);
-		if (head == null) {
-			head = newNode;
-			tail = newNode;
-			return;
-		}
-		tail.next = newNode;
-		newNode.next = head;
-		tail = newNode;
-	}
-
-	void display() {
-		Node temp = head;
-		do {
-			System.out.print(temp.data + " ");
-			temp = temp.next;
-		} while (temp != head);
-	}
-
-	void delete(int val) {
-
-		Node temp = head;
-
-		if (head == null) {
-			return;
-		}
-		if (head == tail) {
-			head = null;
-			tail = null;
-			return;
-		}
-		if (head.data == val) {
-			head = head.next;
-			tail = head;
-			return;
-		}
-
-		while (temp.next.data != val) {
-			temp = temp.next;
-		}
-		temp.next = temp.next.next;
-
-	}
-
-}
+import java.util.Arrays;
 
 class Practice {
 	public static void main(String[] args) {
 
-		CircularLinkedList circularLinkedList = new CircularLinkedList();
+		int[] nums = { 5, 4, 3, 2, 1 };
+		bubbleSort(nums);
+		System.out.println(Arrays.toString(nums));
+	}
 
-		circularLinkedList.insert(0);
-		circularLinkedList.insert(1);
-		circularLinkedList.insert(2);
-		circularLinkedList.insert(3);
-		circularLinkedList.delete(3);
-		circularLinkedList.display();
+	static void bubbleSort(int[] arr) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			boolean isSwap = false;
+			for (int j = 1; j < arr.length; j++) {
+				if (arr[j] < arr[j - 1]) {
+					isSwap = true;
+					swap(arr, j, j - 1);
+				}
+			}
+			if (!isSwap) {
+				break;
+			}
+		}
+	}
+
+	static void swap(int[] arr, int start, int end) {
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
 	}
 
 }
