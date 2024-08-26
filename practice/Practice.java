dfbf6ec028ef893eed90ba4cfb120d0682625db4
Kishore <kishorework77@gmail.com>
2024-08-26 17:06:32 +0530

    practiced quick sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -6,18 +6,33 @@ class Practice {
 	public static void main(String[] args) {
 
 		int[] nums = { 5, 4, 3, 2, 1, 0 };
-		cycleSort(nums);
+		quicksort(nums, 0, nums.length - 1);
 		System.out.println(Arrays.toString(nums));
 	}
 
-	static void cycleSort(int[] arr) {
-
-		for (int i = 0; i < arr.length; i++) {
-			int correctIndex = arr[i] - 1;
-			if (arr[correctIndex] != arr[i]) {
-				swap(arr, i, correctIndex);
+	static void quicksort(int[] arr, int low, int high) {
+		if (low >= high) {
+			return;
+		}
+		int start = low;
+		int end = high;
+		int mid = start + (end - start) / 2;
+		int pivot = arr[mid];
+		while (start <= end) {
+			while (arr[start] < pivot) {
+				start++;
+			}
+			while (arr[end] > pivot) {
+				end--;
+			}
+			if (start <= end) {
+				swap(arr, start, end);
+				start++;
+				end--;
 			}
 		}
+		quicksort(arr, low, end);
+		quicksort(arr, start, high);
 	}
 
 	static void swap(int[] arr, int start, int end) {
