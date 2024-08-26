781ac2026a2dcc6e268d926e06fe85331e583aeb
Kishore <kishorework77@gmail.com>
2024-08-26 13:07:19 +0530

    practiced cyclic sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -5,28 +5,19 @@ import java.util.Arrays;
 class Practice {
 	public static void main(String[] args) {
 
-		int[] nums = { 5, 4, 3, 2, 1 };
-		selectionSort(nums);
+		int[] nums = { 5, 4, 3, 2, 1, 0 };
+		cycleSort(nums);
 		System.out.println(Arrays.toString(nums));
 	}
 
-	static void selectionSort(int[] arr) {
+	static void cycleSort(int[] arr) {
 
 		for (int i = 0; i < arr.length; i++) {
-			int last = arr.length - i - 1;
-			int maxIndex = getMaxIndex(arr, 0, last);
-			swap(arr, maxIndex, last);
-		}
-	}
-
-	static int getMaxIndex(int arr[], int start, int end) {
-		int maxIndex = start;
-		for (int i = start; i <= end; i++) {
-			if (arr[maxIndex] < arr[i]) {
-				maxIndex = i;
+			int correctIndex = arr[i] - 1;
+			if (arr[correctIndex] != arr[i]) {
+				swap(arr, i, correctIndex);
 			}
 		}
-		return maxIndex;
 	}
 
 	static void swap(int[] arr, int start, int end) {
