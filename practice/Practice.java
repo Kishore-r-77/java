4ec0ce77e4fc546ce2e3afddfed3fb8bb5306af8
Kishore <kishorework77@gmail.com>
2024-08-31 12:03:53 +0530

    practiced binary search in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,59 +1,42 @@
 package practice;
 
-class BinarySearchTree {
-
-	Node root;
-
-	private class Node {
-		int data;
-		Node left, right;
-
-		Node(int val) {
-			this.data = val;
-			left = right = null;
-		}
-	}
-
-	BinarySearchTree() {
-
-	}
-
-	BinarySearchTree(int val) {
-		root = new Node(val);
-	}
-
-	Node insertNode(Node root, int val) {
-
-		if (root == null) {
-			return new Node(val);
-		}
-		if (val < root.data) {
-			root.left = insertNode(root.left, val);
-		} else {
-			root.right = insertNode(root.right, val);
-		}
-		return root;
-
-	}
-
-	void preOrderTraversal(Node root) {
-		if (root != null) {
-			System.out.print(root.data + " ");
-			preOrderTraversal(root.left);
-			preOrderTraversal(root.right);
-		}
-
-	}
-}
-
 class Practice {
 	public static void main(String[] args) {
-		BinarySearchTree binaryTree = new BinarySearchTree(10);
 
-		binaryTree.insertNode(binaryTree.root, 6);
-		binaryTree.insertNode(binaryTree.root, 7);
+		int[] arr = { 1, 2, 3, 4, 5 };
 
-		binaryTree.preOrderTraversal(binaryTree.root);
+		System.out.println(binarySearch(arr, 3));
+
+	}
+
+	static int binarySearch(int[] arr, int val) {
+		int start = 0;
+		int end = arr.length - 1;
+
+		boolean isAsc = arr[start] < arr[end];
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+
+			if (arr[mid] == val) {
+				return mid;
+			}
+			if (isAsc) {
+				if (val < arr[mid]) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			} else {
+				if (val < arr[mid]) {
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			}
+
+		}
+		return -1;
 	}
 
 }
