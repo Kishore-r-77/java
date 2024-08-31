25d1facc85736cec248102d896fb0b4069327440
Kishore <kishorework77@gmail.com>
2024-08-31 13:01:24 +0530

    practiced selection sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -7,26 +7,33 @@ class Practice {
 
 		int[] arr = { 5, 4, 3, 2, 1 };
 
-		bubbleSort(arr);
+		selection(arr);
 		System.out.println(Arrays.toString(arr));
 
 	}
 
-	static void bubbleSort(int[] arr) {
+	static void selection(int[] arr) {
 
-		for (int i = 0; i < arr.length - 1; i++) {
-			boolean isSwap = false;
-			for (int j = 1; j < arr.length; j++) {
-				if (arr[j] < arr[j - 1]) {
-					swap(arr, j, j - 1);
-					isSwap = true;
-				}
-			}
-			if (!isSwap) {
-				break;
-			}
+		for (int i = 0; i < arr.length - i; i++) {
+
+			int lastIndex = arr.length - 1 - i;
+			int maxIndex = getMaxIndex(arr, 0, lastIndex);
+			swap(arr, maxIndex, lastIndex);
 
 		}
+
+	}
+
+	static int getMaxIndex(int[] arr, int start, int end) {
+		int maxIndex = start;
+
+		for (int i = start; i <= end; i++) {
+			if (arr[maxIndex] < arr[i]) {
+				maxIndex = i;
+			}
+		}
+
+		return maxIndex;
 	}
 
 	static void swap(int[] arr, int start, int end) {
