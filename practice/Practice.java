31d030fe3d31ac0ba33a19076660e357f91a8f4d
Kishore <kishorework77@gmail.com>
2024-08-31 13:32:45 +0530

    practiced merge sort in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -5,25 +5,48 @@ import java.util.Arrays;
 class Practice {
 	public static void main(String[] args) {
 
-		int[] arr = { 5, 4, 3, 2, 1 };
+		int[] arr = { 5, 4, 6, 8, 7, 3, 2, 9 };
 
-		insertionSort(arr);
+		mergeSort(arr, 0, arr.length);
 		System.out.println(Arrays.toString(arr));
 
 	}
 
-	static void insertionSort(int[] arr) {
+	static void mergeSort(int[] arr, int start, int end) {
 
-		for (int i = 0; i < arr.length - 1; i++) {
+		if (end - start < 2) {
+			return;
+		}
 
-			for (int j = i + 1; j > 0; j--) {
-				if (arr[j] < arr[j - 1]) {
-					swap(arr, j, j - 1);
-				} else {
-					break;
-				}
+		int mid = start + (end - start) / 2;
+
+		mergeSort(arr, start, mid);
+		mergeSort(arr, mid, end);
+		merge(arr, start, mid, end);
+
+	}
+
+	static void merge(int[] arr, int start, int mid, int end) {
+		int joined[] = new int[end - start];
+
+		int i = start, j = mid, k = 0;
+
+		while (i < mid && j < end) {
+			if (arr[i] < arr[j]) {
+				joined[k++] = arr[i++];
+			} else {
+				joined[k++] = arr[j++];
 			}
+		}
+		while (i < mid) {
+			joined[k++] = arr[i++];
+		}
+		while (j < end) {
+			joined[k++] = arr[j++];
+		}
 
+		for (int index = 0; index < joined.length; index++) {
+			arr[start + index] = joined[index];
 		}
 
 	}
