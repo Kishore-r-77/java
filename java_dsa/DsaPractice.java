7a78e65f3dd1055fcc2dd03cee280c5d5d93a794
Kishore <kishorework77@gmail.com>
2024-09-02 15:58:45 +0530

    practiced avl tree in java

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -1,45 +1,117 @@
 package java_dsa;
 
-class BinarySearchTree {
+class Avl {
 
 	Node root;
 
 	private class Node {
 		int data;
 		Node left, right;
+		int height;
 
 		Node(int val) {
 			this.data = val;
 			this.right = null;
 			this.left = null;
+			this.height = 0;
 		}
 	}
 
-	BinarySearchTree() {
-
+	Avl() {
+		this.root = null;
 	}
 
-	BinarySearchTree(int val) {
+	Avl(int val) {
 		this.root = new Node(val);
+	}
+
+	int getHeight(Node root) {
+		if (root == null) {
+			return -1;
+		}
+		return root.height;
+	}
+
+	int max(int a, int b) {
+		return a > b ? a : b;
+	}
+
+	int getBalanceFactor(Node node) {
+		if (node == null) {
+			return 0;
+		}
+		return getHeight(node.left) - getHeight(node.right);
 	}
 
 	Node insertNode(Node root, int val) {
 		if (root == null) {
-			root = new Node(val);
-			return root;
+			return new Node(val);
 		}
 
 		if (val < root.data) {
 			root.left = insertNode(root.left, val);
-		} else {
+		} else if (val > root.data) {
 			root.right = insertNode(root.right, val);
+		} else {
+			return root;
+		}
+
+		root.height = 1 + max(getHeight(root.left), getHeight(root.right));
+
+		int balanceFactor = getBalanceFactor(root);
+
+		// LL Rotation
+		if (balanceFactor > 1 && val < root.left.data) {
+			return rightRotate(root);
+		}
+
+		// LR Rotation
+		if (balanceFactor > 1 && val > root.left.data) {
+			root.left = leftRotate(root.left);
+			return rightRotate(root);
+		}
+
+		// RR Rotation
+		if (balanceFactor < -1 && val > root.right.data) {
+			return leftRotate(root);
+		}
+
+		// RL Rotation
+		if (balanceFactor < -1 && val < root.right.data) {
+			root.right = rightRotate(root.right);
+			return leftRotate(root);
 		}
 
 		return root;
 	}
 
-	Node delete(Node root, int val) {
+	Node rightRotate(Node z) {
+		Node y = z.left;
+		Node t3 = y.right;
 
+		y.right = z;
+		z.left = t3;
+
+		z.height = 1 + max(getHeight(z.left), getHeight(z.right));
+		y.height = 1 + max(getHeight(y.left), getHeight(y.right));
+
+		return y;
+	}
+
+	Node leftRotate(Node z) {
+		Node y = z.right;
+		Node t3 = y.left;
+
+		y.left = z;
+		z.right = t3;
+
+		z.height = 1 + max(getHeight(z.left), getHeight(z.right));
+		y.height = 1 + max(getHeight(y.left), getHeight(y.right));
+
+		return y;
+	}
+
+	Node delete(Node root, int val) {
 		if (root == null) {
 			return root;
 		}
@@ -54,9 +126,35 @@ class BinarySearchTree {
 			} else if (root.right == null) {
 				return root.left;
 			}
+
 			root.data = min(root.right);
 			root.right = delete(root.right, root.data);
+		}
 
+		root.height = 1 + max(getHeight(root.left), getHeight(root.right));
+
+		int balanceFactor = getBalanceFactor(root);
+
+		// LL Rotation
+		if (balanceFactor > 1 && getBalanceFactor(root.left) >= 0) {
+			return rightRotate(root);
+		}
+
+		// LR Rotation
+		if (balanceFactor > 1 && getBalanceFactor(root.left) < 0) {
+			root.left = leftRotate(root.left);
+			return rightRotate(root);
+		}
+
+		// RR Rotation
+		if (balanceFactor < -1 && getBalanceFactor(root.right) <= 0) {
+			return leftRotate(root);
+		}
+
+		// RL Rotation
+		if (balanceFactor < -1 && getBalanceFactor(root.right) > 0) {
+			root.right = rightRotate(root.right);
+			return leftRotate(root);
 		}
 
 		return root;
@@ -64,14 +162,11 @@ class BinarySearchTree {
 
 	int min(Node root) {
 		int minVal = root.data;
-
 		while (root.left != null) {
-			minVal = root.data;
+			minVal = root.left.data;
 			root = root.left;
-
 		}
 		return minVal;
-
 	}
 
 	void preOrder(Node root) {
@@ -81,22 +176,26 @@ class BinarySearchTree {
 			preOrder(root.right);
 		}
 	}
+
+	void inOrder(Node root) {
+		if (root != null) {
+			inOrder(root.left);
+			System.out.print(root.data + " ");
+			inOrder(root.right);
+		}
+	}
 }
 
 public class DsaPractice {
 
 	public static void main(String[] args) {
-		BinarySearchTree binarySearchTree = new BinarySearchTree(50);
-		binarySearchTree.insertNode(binarySearchTree.root, 20);
-		binarySearchTree.insertNode(binarySearchTree.root, 70);
-		binarySearchTree.insertNode(binarySearchTree.root, 10);
-		binarySearchTree.insertNode(binarySearchTree.root, 25);
-		binarySearchTree.insertNode(binarySearchTree.root, 60);
-		binarySearchTree.insertNode(binarySearchTree.root, 90);
-		binarySearchTree.insertNode(binarySearchTree.root, 80);
-		binarySearchTree.insertNode(binarySearchTree.root, 75);
-		binarySearchTree.delete(binarySearchTree.root, 70);
-		binarySearchTree.preOrder(binarySearchTree.root);
-	}
+		Avl avltree = new Avl(50);
+		avltree.root = avltree.insertNode(avltree.root, 20);
+		avltree.root = avltree.insertNode(avltree.root, 60);
+		avltree.root = avltree.insertNode(avltree.root, 10);
+		avltree.root = avltree.insertNode(avltree.root, 30);
+		avltree.root = avltree.insertNode(avltree.root, 40);
 
+		avltree.inOrder(avltree.root);
+	}
 }
