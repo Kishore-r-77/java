6099d85d918909cc3a6e121fd85286a4f1f32d81
Kishore <kishorework77@gmail.com>
2024-09-03 16:51:51 +0530

    worked out leetcode 1108 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,93 +1,31 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-class Graph {
-
-	List<List<Integer>> adjList = new ArrayList<>();
-
-	Graph(int val) {
-		for (int i = 0; i < val; i++) {
-			adjList.add(new ArrayList<>());
-		}
-	}
-
-	void addEdge(int val1, int val2) {
-		adjList.get(val1).add(val2);
-		adjList.get(val2).add(val1);
-	}
-
-	void display() {
-		for (int i = 0; i < adjList.size(); i++) {
-			System.out.println("For vertice " + i + " the lists are");
-			for (int j = 0; j < adjList.get(i).size(); j++) {
-				System.out.println(adjList.get(i).get(j) + " ");
-			}
-		}
-	}
-
-	void bfs(int v) {
-		int V = adjList.size();
-		boolean visited[] = new boolean[V];
-
-		visited[v] = true;
-
-		Queue<Integer> queue = new LinkedList<>();
-
-		queue.add(v);
-
-		while (queue.size() != 0) {
-			Integer dequeue = queue.remove();
-
-			System.out.print(dequeue + " ");
-
-			for (int i = 0; i < adjList.get(dequeue).size(); i++) {
-				Integer av = adjList.get(dequeue).get(i);
-
-				if (!visited[av]) {
-					queue.add(av);
-					visited[av] = true;
-				}
-			}
-		}
-
-	}
-
-	void dfs(int v) {
-		int V = adjList.size();
-		boolean visited[] = new boolean[V];
-		dfs2(v, visited);
-	}
-
-	void dfs2(int v, boolean[] visited) {
-		visited[v] = true;
-
-		System.out.print(v + " ");
-
-		for (int i = 0; i < adjList.get(v).size(); i++) {
-
-			Integer av = adjList.get(v).get(i);
-			if (!visited[av]) {
-				dfs2(av, visited);
-			}
-		}
-	}
-}
-
 class Practice {
 	public static void main(String[] args) {
-		Graph graph = new Graph(5);
 
-		graph.addEdge(0, 1);
-		graph.addEdge(2, 3);
-		graph.addEdge(1, 2);
-		graph.addEdge(3, 4);
-		graph.addEdge(0, 4);
-		graph.addEdge(1, 4);
-		graph.dfs(0);
+		// Input: address = "1.1.1.1"
+		// Output: "1[.]1[.]1[.]1"
+
+		String address = "1.1.1.1";
+
+		System.out.println(defangIPaddr(address));
+
+	}
+
+	static public String defangIPaddr(String address) {
+		String output = "";
+
+		char[] charArray = address.toCharArray();
+
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] == '.') {
+				output += "[.]";
+			} else {
+				output += charArray[i] + "";
+			}
+		}
+
+		return output;
 	}
 
 }
