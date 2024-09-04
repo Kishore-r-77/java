b00534a42403ddf3c8d1a0cb09fd42904a1d11db
Kishore <kishorework77@gmail.com>
2024-09-04 10:57:49 +0530

    worked out leetcode 1678 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,29 +3,34 @@ package practice;
 class Practice {
 	public static void main(String[] args) {
 
-		// Input: address = "1.1.1.1"
-		// Output: "1[.]1[.]1[.]1"
+		// Input: command = "G()(al)"
+		// Output: "Goal"
 
-		String address = "1.1.1.1";
+		String command = "G()(al)";
 
-		System.out.println(defangIPaddr(address));
+		System.out.println(interpret(command));
 
 	}
 
-	static public String defangIPaddr(String address) {
+	static public String interpret(String command) {
+
 		String output = "";
 
-		char[] charArray = address.toCharArray();
+		char[] charArray = command.toCharArray();
 
 		for (int i = 0; i < charArray.length; i++) {
-			if (charArray[i] == '.') {
-				output += "[.]";
-			} else {
-				output += charArray[i] + "";
+			if (charArray[i] == 'G') {
+				output += "G";
+			} else if (charArray[i] == '(' && charArray[i + 1] == ')') {
+				output += "o";
+			} else if (charArray[i] == '(' && charArray[i + 1] == 'a' && charArray[i + 2] == 'l'
+					&& charArray[i + 3] == ')') {
+				output += "al";
 			}
-		}
 
+		}
 		return output;
+
 	}
 
 }
