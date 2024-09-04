09dd13471aa09592e8297223e765507a71b69e42
Kishore <kishorework77@gmail.com>
2024-09-04 14:25:21 +0530

    worked out leetcode 1773 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,36 +1,50 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Practice {
 	public static void main(String[] args) {
 
-		// Input: command = "G()(al)"
-		// Output: "Goal"
+		// Input: items =
+		// [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]],
+		// ruleKey = "color", ruleValue = "silver"
+		// Output: 1
 
-		String command = "G()(al)";
+		List<List<String>> items = new ArrayList<>(
+				new ArrayList<>(List.of(List.of("phone", "blue", "pixel"), List.of("computer", "silver", "lenovo"),
+						List.of("phone", "gold", "iphone"))));
 
-		System.out.println(interpret(command));
+		System.out.println(countMatches(items, "color", "silver"));
 
 	}
 
-	static public String interpret(String command) {
-
-		String output = "";
-
-		char[] charArray = command.toCharArray();
-
-		for (int i = 0; i < charArray.length; i++) {
-			if (charArray[i] == 'G') {
-				output += "G";
-			} else if (charArray[i] == '(' && charArray[i + 1] == ')') {
-				output += "o";
-			} else if (charArray[i] == '(' && charArray[i + 1] == 'a' && charArray[i + 2] == 'l'
-					&& charArray[i + 3] == ')') {
-				output += "al";
+	static public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+		int count = 0;
+		if (ruleKey.equalsIgnoreCase("type")) {
+			for (int i = 0; i < items.size(); i++) {
+				String typeValue = items.get(i).get(0);
+				if (ruleValue.equals(typeValue)) {
+					count++;
+				}
 			}
-
+		} else if (ruleKey.equalsIgnoreCase("color")) {
+			for (int i = 0; i < items.size(); i++) {
+				String typeValue = items.get(i).get(1);
+				if (ruleValue.equals(typeValue)) {
+					count++;
+				}
+			}
+		} else {
+			for (int i = 0; i < items.size(); i++) {
+				String typeValue = items.get(i).get(2);
+				if (ruleValue.equals(typeValue)) {
+					count++;
+				}
+			}
 		}
-		return output;
 
+		return count;
 	}
 
 }
