b092bdf3995b14d7151a19a7196948f6725da33d
Kishore <kishorework77@gmail.com>
2024-09-07 17:08:34 +0530

    worked out leetcode 28 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,50 +1,25 @@
 package practice;
 
-import java.util.Arrays;
-
 class Practice {
 	public static void main(String[] args) {
 
-		// Input: s = "Let's take LeetCode contest"
-		// Output: "s'teL ekat edoCteeL tsetnoc"
+		// Input: haystack = "sadbutsad", needle = "sad"
+		// Output: 0
 
-		String s = "Let's take LeetCode contest";
+		String haystack = "sadbutsad";
+		String needle = "sad";
 
-		System.out.println(reverseWords(s));
-
-	}
-
-	static public String reverseWords(String s) {
-
-		String output = "";
-
-		String[] stringArray = s.split(" ");
-
-		for (String word : stringArray) {
-			output += reverse(word) + " ";
-		}
-
-		return output.trim();
+		System.out.println(strStr(haystack, needle));
 
 	}
 
-	static public String reverse(String s) {
-		char[] charArray = s.toCharArray();
+	static public int strStr(String haystack, String needle) {
 
-		int start = 0;
-		int end = s.length() - 1;
-		String output = "";
+		if (haystack.contains(needle)) {
+			return haystack.indexOf(needle);
+		}
 
-		while (start <= end) {
-			char temp = charArray[start];
-			charArray[start] = charArray[end];
-			charArray[end] = temp;
-			start++;
-			end--;
-		}
-		for (int i = 0; i < charArray.length; i++) {
-			output += charArray[i];
-		}
-		return output;
+		return -1;
 	}
+
 }
