023863426feb4d85b6c42014bf49370efccb6d81
Kishore <kishorework77@gmail.com>
2024-09-08 22:33:10 +0530

    worked out leetcode 14 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,51 +3,42 @@ package practice;
 class Practice {
 	public static void main(String[] args) {
 
-		// Input: s = "aba"
-		// Output: true
+		// Input: strs = ["flower","flow","flight"]
+		// Output: "fl"
 
-		String s = "abca";
+		String[] strs = { "flower", "flow", "flight" };
 
-		System.out.println(validPalindrome(s));
-
-	}
-
-	static public boolean validPalindrome(String s) {
-
-		int left = 0;
-		int right = s.length() - 1;
-
-		while (left < right) {
-			if (s.charAt(left) != s.charAt(right)) {
-				if (isPalindrome(left + 1, right, s)) {
-					return true;
-				}
-				if (isPalindrome(left, right - 1, s)) {
-					return true;
-				}
-				return false;
-			}
-			left++;
-			right--;
-
-		}
-
-		return true;
+		System.out.println(longestCommonPrefix(strs));
 
 	}
 
-	static public boolean isPalindrome(int left, int right, String s) {
-
-		while (left < right) {
-			if (s.charAt(left) != s.charAt(right)) {
-				return false;
-			}
-			left++;
-			right--;
-
+	static public String longestCommonPrefix(String[] strs) {
+		if (strs == null || strs.length == 0) {
+			return "";
 		}
 
-		return true;
+		// Step 1: Assume the first string is the longest common prefix
+		String prefix = strs[0];
+
+		// Step 2: Compare the prefix with each subsequent string
+		for (int i = 1; i < strs.length; i++) {
+			// Step 3: Shorten the prefix as long as it's not a prefix of the current string
+
+			while (strs[i].indexOf(prefix) != 0) {
+				// Remove the last character from the prefix
+
+				prefix = prefix.substring(0, prefix.length() - 1);
+
+				// If the prefix becomes empty, there's no common prefix
+				if (prefix.isEmpty()) {
+					return "";
+				}
+			}
+		}
+
+		// Step 4: Return the longest common prefix
+		return prefix;
+
 	}
 
 }
