0766167505909ec0aa5fe461211bf6e780dfd7cd
Kishore <kishorework77@gmail.com>
2024-09-08 15:16:23 +0530

    worked out leetcode 680 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,32 +3,48 @@ package practice;
 class Practice {
 	public static void main(String[] args) {
 
-		// Input: s = "A man, a plan, a canal: Panama"
+		// Input: s = "aba"
 		// Output: true
 
-		String s = "A man, a plan, a canal: Panama";
+		String s = "abca";
 
-		System.out.println(isPalindrome(s));
+		System.out.println(validPalindrome(s));
 
 	}
 
-	static public boolean isPalindrome(String s) {
+	static public boolean validPalindrome(String s) {
 
 		int left = 0;
 		int right = s.length() - 1;
 
 		while (left < right) {
-			while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-				left++;
-			}
-			while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-				right--;
-			}
-			if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+			if (s.charAt(left) != s.charAt(right)) {
+				if (isPalindrome(left + 1, right, s)) {
+					return true;
+				}
+				if (isPalindrome(left, right - 1, s)) {
+					return true;
+				}
 				return false;
 			}
 			left++;
 			right--;
+
+		}
+
+		return true;
+
+	}
+
+	static public boolean isPalindrome(int left, int right, String s) {
+
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+
 		}
 
 		return true;
