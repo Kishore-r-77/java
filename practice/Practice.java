7cc3bc1f880ba2d6f28652f9e9c45c9852d8dfcd
Kishore <kishorework77@gmail.com>
2024-09-09 10:51:33 +0530

    worked out leetcode 1470 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -5,34 +5,26 @@ import java.util.Arrays;
 class Practice {
 	public static void main(String[] args) {
 
-		// Input: accounts = [[1,2,3],[3,2,1]]
-		// Output: 6
+		// Input: nums = [2,5,1,3,4,7], n = 3
+		// Output: [2,3,5,4,1,7]
 
-		int[][] nums = {
-				{ 1, 2, 3 },
-				{ 3, 2, 1 },
+		int[] nums = {
+				2, 5, 1, 3, 4, 7
 		};
 
-		System.out.println(maximumWealth(nums));
+		System.out.println(Arrays.toString(shuffle(nums, 3)));
 
 	}
 
-	static public int maximumWealth(int[][] accounts) {
+	static public int[] shuffle(int[] nums, int n) {
+		int[] output = new int[nums.length];
 
-		int max = 0;
-
-		for (int i = 0; i < accounts.length; i++) {
-			int sum = 0;
-			for (int j = 0; j < accounts[0].length; j++) {
-				sum += accounts[i][j];
-				if (sum > max) {
-					max = sum;
-				}
-			}
-
+		for (int i = 0; i < n; i++) {
+			output[2 * i] = nums[i];
+			output[2 * i + 1] = nums[i + n];
 		}
 
-		return max;
+		return output;
 	}
 
 }
