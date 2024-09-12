62e93fd3f542128825b5a0589f09a3c12ebcfe60
Kishore <kishorework77@gmail.com>
2024-09-12 18:27:09 +0530

    practiced leetcode 54 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,33 +1,64 @@
 package practice;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Practice {
     public static void main(String[] args) {
-        // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-        // Output: [[1,4,7],[2,5,8],[3,6,9]]
 
-        int[][] nums = {
+        // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        // Output: [1,2,3,6,9,8,7,4,5]
+
+        int[][] matrix = {
                 { 1, 2, 3 },
                 { 4, 5, 6 },
                 { 7, 8, 9 },
         };
 
-        System.out.println(Arrays.deepToString(transpose(nums)));
-
+        System.out.println(spiralOrder(matrix));
     }
 
-    static public int[][] transpose(int[][] matrix) {
+    static public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
 
-        int result[][] = new int[matrix[0].length][matrix.length];
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
 
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[0].length; j++) {
-                result[j][i] = matrix[i][j];
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        int up = 0;
+        int down = matrix.length - 1;
+
+        // left to right
+        while (left <= right && up <= down) {
+            for (int i = left; i <= right; i++) {
+
+                result.add((matrix[up][i]));
+            }
+            up++;
+
+            for (int i = up; i <= down; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            if (up <= down) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[down][i]);
+                }
+                down--;
+            }
+            if (left <= right) {
+                for (int i = down; i >= up; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
             }
         }
 
         return result;
+
     }
 
 }
