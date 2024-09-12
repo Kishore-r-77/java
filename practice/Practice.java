3d62648d21116de04b262a412dbe3a07761fb04e
Kishore <kishorework77@gmail.com>
2024-09-12 08:41:17 +0530

    practiced leetcode 1252 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,46 +1,49 @@
 package practice;
 
-import java.util.Arrays;
-
 class Practice {
     public static void main(String[] args) {
+        // Input: m = 2, n = 3, indices = [[0,1],[1,1]]
+        // Output: 6
 
-        // Input: image = [[1,1,0],[1,0,1],[0,0,0]]
-        // Output: [[1,0,0],[0,1,0],[1,1,1]]
+        int m = 2;
+        int n = 3;
 
-        int[][] image = {
-                { 1, 1, 0 },
-                { 1, 0, 1 },
-                { 0, 0, 0 },
+        int[][] indices = {
+                { 0, 1 },
+                { 1, 1 },
         };
 
-        System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
+        System.out.println(oddCells(m, n, indices));
 
     }
 
-    static public int[][] flipAndInvertImage(int[][] image) {
+    static public int oddCells(int m, int n, int[][] indices) {
+        int oddCount = 0;
 
-        for (int i = 0; i < image.length; i++) {
-            swap(image[i], 0, image.length - 1);
+        int[][] matrix = new int[m][n];
+
+        for (int[] index : indices) {
+
+            int row = index[0];
+            int col = index[1];
+
+            for (int i = 0; i < n; i++) {
+                matrix[row][i] += 1;
+            }
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] += 1;
+            }
 
         }
-        for (int i = 0; i < image.length; i++) {
-            for (int j = 0; j < image.length; j++) {
-                image[i][j] = image[i][j] ^ 1;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] % 2 != 0) {
+                    oddCount++;
+                }
             }
         }
 
-        return image;
+        return oddCount;
     }
-
-    static void swap(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
 }
