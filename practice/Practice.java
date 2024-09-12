ab3c67ca4ac019b7ad78427e1525f410c9051314
Kishore <kishorework77@gmail.com>
2024-09-12 08:51:21 +0530

    practiced leetcode 1572 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -2,48 +2,34 @@ package practice;
 
 class Practice {
     public static void main(String[] args) {
-        // Input: m = 2, n = 3, indices = [[0,1],[1,1]]
-        // Output: 6
+        // Input: mat = [[1,2,3],
+        // [4,5,6],
+        // [7,8,9]]
+        // Output: 25
 
-        int m = 2;
-        int n = 3;
-
-        int[][] indices = {
-                { 0, 1 },
-                { 1, 1 },
+        int[][] mat = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
         };
 
-        System.out.println(oddCells(m, n, indices));
+        System.out.println(diagonalSum(mat));
 
     }
 
-    static public int oddCells(int m, int n, int[][] indices) {
-        int oddCount = 0;
+    static public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
 
-        int[][] matrix = new int[m][n];
-
-        for (int[] index : indices) {
-
-            int row = index[0];
-            int col = index[1];
-
-            for (int i = 0; i < n; i++) {
-                matrix[row][i] += 1;
-            }
-            for (int i = 0; i < m; i++) {
-                matrix[i][col] += 1;
-            }
-
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - i - 1];
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] % 2 != 0) {
-                    oddCount++;
-                }
-            }
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
         }
 
-        return oddCount;
+        return sum;
     }
 }
