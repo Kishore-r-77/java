bc2f6ad7aded25d5ceedf72541a60580edfdda93
Kishore <kishorework77@gmail.com>
2024-09-18 13:16:25 +0530

    practiced leetcode 1108 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,63 +1,28 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Practice {
     public static void main(String[] args) {
+        // Input: address = "1.1.1.1"
+        // Output: "1[.]1[.]1[.]1"
 
-        // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-        // Output: [1,2,3,6,9,8,7,4,5]
+        String address = "1.1.1.1";
 
-        int[][] matrix = {
-                { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 },
-        };
-
-        System.out.println(spiralOrder(matrix));
+        System.out.println(defangIPaddr(address));
     }
 
-    static public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
+    static public String defangIPaddr(String address) {
 
-        if (matrix == null || matrix.length == 0) {
-            return result;
-        }
+        StringBuilder sb = new StringBuilder();
 
-        int left = 0;
-        int right = matrix[0].length - 1;
-
-        int up = 0;
-        int down = matrix.length - 1;
-
-        // left to right
-        while (left <= right && up <= down) {
-            for (int i = left; i <= right; i++) {
-
-                result.add((matrix[up][i]));
-            }
-            up++;
-
-            for (int i = up; i <= down; i++) {
-                result.add(matrix[i][right]);
-            }
-            right--;
-            if (up <= down) {
-                for (int i = right; i >= left; i--) {
-                    result.add(matrix[down][i]);
-                }
-                down--;
-            }
-            if (left <= right) {
-                for (int i = down; i >= up; i--) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(address.charAt(i));
             }
         }
 
-        return result;
+        return sb.toString();
 
     }
 
