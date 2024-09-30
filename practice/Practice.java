5a50b4970c409b0e457d9147c29857a81107807a
Kishore <kishorework77@gmail.com>
2024-09-30 17:27:09 +0530

    practiced leetcode 1871 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,27 +1,53 @@
 package practice;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 58
+        // leetcode 1871
 
-        // Input: s = " fly me to the moon "
-        // Output: 4
-        // Explanation: The last word is "moon" with length 4.
+        // Input: s = "011010", minJump = 2, maxJump = 3
+        // Output: true
+        // Explanation:
+        // In the first step, move from index 0 to index 3.
+        // In the second step, move from index 3 to index 5.
 
-        String s = " fly me to the moon ";
+        String s = "011010";
 
-        System.out.println(lengthOfLastWord(s));
+        System.out.println(canReach(s, 2, 3));
 
     }
 
-    static public int lengthOfLastWord(String s) {
+    static public boolean canReach(String s, int minJump, int maxJump) {
 
-        s = s.trim();
+        int n = s.length();
+        if (s.charAt(n - 1) != '0')
+            return false;
 
-        String[] wordArray = s.split(" ");
+        Queue<Integer> queue = new LinkedList<>();
 
-        return wordArray[wordArray.length - 1].length();
+        queue.add(0);
 
+        int maxReach = 0;
+
+        while (!queue.isEmpty()) {
+            int idx = queue.remove();
+            if (idx == n - 1) {
+                return true;
+            }
+
+            for (int i = Math.max(idx + minJump, maxReach); i <= Math.min(idx + maxJump, n - 1); i++) {
+                if (s.charAt(i) == '0') {
+                    queue.add(i);
+                }
+
+            }
+            maxReach = Math.min(idx + maxJump, n - 1);
+
+        }
+
+        return false;
     }
 }
