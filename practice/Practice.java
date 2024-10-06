28d4716ed09557658fe50593f20cf9e8803ed289
Kishore <kishorework77@gmail.com>
2024-10-06 13:32:40 +0530

    Practiced leetcode 1920 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,53 +1,34 @@
 package practice;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 1871
+        // leetcode 1920
 
-        // Input: s = "011010", minJump = 2, maxJump = 3
-        // Output: true
-        // Explanation:
-        // In the first step, move from index 0 to index 3.
-        // In the second step, move from index 3 to index 5.
+        // Input: nums = [0,2,1,5,3,4]
+        // Output: [0,1,2,4,5,3]
+        // Explanation: The array ans is built as follows:
+        // ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]],
+        // nums[nums[4]], nums[nums[5]]]
+        // = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+        // = [0,1,2,4,5,3]
 
-        String s = "011010";
+        int[] nums = { 0, 2, 1, 5, 3, 4 };
 
-        System.out.println(canReach(s, 2, 3));
+        System.out.println(Arrays.toString(buildArray(nums)));
 
     }
 
-    static public boolean canReach(String s, int minJump, int maxJump) {
+    static public int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
 
-        int n = s.length();
-        if (s.charAt(n - 1) != '0')
-            return false;
-
-        Queue<Integer> queue = new LinkedList<>();
-
-        queue.add(0);
-
-        int maxReach = 0;
-
-        while (!queue.isEmpty()) {
-            int idx = queue.remove();
-            if (idx == n - 1) {
-                return true;
-            }
-
-            for (int i = Math.max(idx + minJump, maxReach); i <= Math.min(idx + maxJump, n - 1); i++) {
-                if (s.charAt(i) == '0') {
-                    queue.add(i);
-                }
-
-            }
-            maxReach = Math.min(idx + maxJump, n - 1);
-
+        for (int i = 0; i < result.length; i++) {
+            result[i] = nums[nums[i]];
         }
 
-        return false;
+        return result;
     }
+
 }
