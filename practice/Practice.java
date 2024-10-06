d1a33d76741175d6d4d400c31508bb0ba7c8dc63
Kishore <kishorework77@gmail.com>
2024-10-06 14:47:59 +0530

    Practiced leetcode 1512 with o(n) in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -24,13 +24,10 @@ class Practice {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            map.put(i, nums[i]);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsValue(nums[i])) {
-                count += 1;
+            if (map.containsKey(nums[i])) {
+                count += map.get(nums[i]);
             }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
         return count;
