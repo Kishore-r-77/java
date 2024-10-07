a54bd399e0d4b6be5b32c210b4c582dfb46fb6bb
Kishore <kishorework77@gmail.com>
2024-10-07 09:57:32 +0530

    Practiced leetcode 1365 with o(n^2) in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,36 +1,43 @@
 package practice;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 1512
+        // leetcode 1365
 
-        // Input: nums = [1,2,3,1,1,3]
-        // Output: 4
-        // Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+        // Input: nums = [8,1,2,2,3]
+        // Output: [4,0,1,1,3]
+        // Explanation:
+        // For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+        // For nums[1]=1 does not exist any smaller number than it.
+        // For nums[2]=2 there exist one smaller number than it (1).
+        // For nums[3]=2 there exist one smaller number than it (1).
+        // For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
 
-        int[] nums = { 1, 2, 3, 1, 1, 3 };
+        int[] nums = { 8, 1, 2, 2, 3 };
 
-        System.out.println(numIdenticalPairs(nums));
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
 
     }
 
-    static public int numIdenticalPairs(int[] nums) {
-        int count = 0;
+    static public int[] smallerNumbersThanCurrent(int[] nums) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+        int result[] = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                count += map.get(nums[i]);
+        for (int i = 0; i < result.length; i++) {
+            int count = 0;
+            for (int j = 0; j < result.length; j++) {
+                if (nums[i] > nums[j]) {
+                    count += 1;
+                }
             }
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            result[i] = count;
         }
 
-        return count;
+        return result;
+
     }
 
 }
