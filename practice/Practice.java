64452bb6f388e7f1a66b6cb4f1c4143daf7efe21
Kishore <kishorework77@gmail.com>
2024-10-08 16:19:04 +0530

    Practiced leetcode 832 with o(n^2) in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,35 +1,51 @@
 package practice;
 
-import java.util.List;
+import java.util.Arrays;
 
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 1732
+        // leetcode 832
 
-        // Input: gain = [-5,1,5,0,-7]
-        // Output: 1
-        // Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+        // Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+        // Output: [[1,0,0],[0,1,0],[1,1,1]]
+        // Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+        // Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
 
-        int[] gain = { -5, 1, 5, 0, -7 };
+        int[][] image = {
+                { 1, 1, 0 },
+                { 1, 0, 1 },
+                { 0, 0, 0 },
+        };
 
-        System.out.println(largestAltitude(gain));
+        System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
 
     }
 
-    static public int largestAltitude(int[] gain) {
-        int altitude = 0;
-        int max = 0;
+    static public int[][] flipAndInvertImage(int[][] image) {
 
-        for (int i = 0; i < gain.length; i++) {
-            altitude += gain[i];
-            if (altitude > max) {
-                max = altitude;
+        for (int i = 0; i < image.length; i++) {
+            swap(0, image[i].length - 1, image[i]);
+        }
+
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                image[i][j] ^= 1;
             }
         }
 
-        return max;
+        return image;
+    }
 
+    static public void swap(int start, int end, int[] arr) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
     }
 
 }
