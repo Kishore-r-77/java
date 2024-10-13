3f10420334f71362eae5bc3773f14f75ec441d16
Kishore <kishorework77@gmail.com>
2024-10-13 16:53:11 +0530

    Practiced leetcode 1252 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,51 +1,55 @@
 package practice;
 
-import java.util.Arrays;
-
 class Practice {
     public static void main(String[] args) {
 
         // leetcode 832
 
-        // Input: image = [[1,1,0],[1,0,1],[0,0,0]]
-        // Output: [[1,0,0],[0,1,0],[1,1,1]]
-        // Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
-        // Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+        // Input: m = 2, n = 3, indices = [[0,1],[1,1]]
+        // Output: 6
+        // Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+        // After applying first increment it becomes [[1,2,1],[0,1,0]].
+        // The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.
 
-        int[][] image = {
-                { 1, 1, 0 },
-                { 1, 0, 1 },
-                { 0, 0, 0 },
+        int[][] indices = {
+                { 0, 1 },
+                { 1, 1 },
         };
 
-        System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
+        // {
+        // {1,3,1},
+        // {1,3,1},
+        // }
+
+        System.out.println(oddCells(2, 3, indices));
 
     }
 
-    static public int[][] flipAndInvertImage(int[][] image) {
+    static public int oddCells(int m, int n, int[][] indices) {
+        int[][] matrix = new int[m][n];
+        int count = 0;
 
-        for (int i = 0; i < image.length; i++) {
-            swap(0, image[i].length - 1, image[i]);
-        }
+        for (int[] index : indices) {
+            int row = index[0];
+            int col = index[1];
 
-        for (int i = 0; i < image.length; i++) {
-            for (int j = 0; j < image[i].length; j++) {
-                image[i][j] ^= 1;
+            for (int i = 0; i < n; i++) {
+                matrix[row][i]++;
+            }
+            for (int i = 0; i < m; i++) {
+                matrix[i][col]++;
             }
         }
 
-        return image;
-    }
+        for (int[] row : matrix) {
+			for (int num : row) {
+				if (num % 2 != 0) {
+					count++;
+				}
+			}
+		}
 
-    static public void swap(int start, int end, int[] arr) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-
-        }
+        return count;
     }
 
 }
