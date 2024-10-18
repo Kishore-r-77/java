b3834b66004187ee8a387a038fa99a9f0bc18141
Kishore <kishorework77@gmail.com>
2024-10-18 13:43:33 +0530

    Practiced leetcode 1886 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,45 +3,76 @@ package practice;
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 1854
+        // leetcode 1886
 
-        // Input: logs = [[1993,1999],[2000,2010]]
-        // Output: 1993
-        // Explanation: The maximum population is 1, and 1993 is the earliest year with
-        // this population.
+        // Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+        // Output: true
 
-        int[][] logs = {
-                { 1993, 1999 },
-                { 2000, 2010 },
+        int[][] mat = {
+                { 0, 1 },
+                { 1, 0 },
+        };
+        int[][] target = {
+                { 1, 0 },
+                { 0, 1 },
         };
 
-        System.out.println(maximumPopulation(logs));
+        // [[1, 2],
+        // [3, 4]]
+
+        // [[3, 1],
+        // [4, 2]]
+
+        // 4 3
+        // 2 1
+
+        System.out.println(findRotation(mat, target));
 
     }
 
-    static public int maximumPopulation(int[][] logs) {
-        int[] populationChanges = new int[101];
-
-        for (int log[] : logs) {
-            int birth = log[0] - 1950;
-            int death = log[1] - 1950;
-            populationChanges[birth]++;
-            populationChanges[death]--;
+    static public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            if (equals(mat, target)) {
+                return true;
+            }
+            rotate(mat);
         }
 
-        int maxPopulation = 0;
-        int currentPopulation = 0;
-        int year = 1950;
+        return false;
 
-        for (int i = 0; i < populationChanges.length; i++) {
-            currentPopulation += populationChanges[i];
-            if (currentPopulation > maxPopulation) {
-                maxPopulation = currentPopulation;
-                year = 1950 + i;
+    }
+
+    static public void rotate(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = i + 1; j < mat.length; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
 
-        return year;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length / 2; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][mat.length - 1 - j];
+                mat[i][mat.length - 1 - j] = temp;
+            }
+        }
+
+    }
+
+    static public boolean equals(int[][] mat, int[][] target) {
+
+        for (int i = 0; i < target.length; i++) {
+            for (int j = 0; j < target[0].length; j++) {
+                if (mat[i][j] != target[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
     }
 
 }
