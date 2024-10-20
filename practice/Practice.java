75e895d351868c31ec46286cefe989db9e8459f2
Kishore <kishorework77@gmail.com>
2024-10-20 19:33:25 +0530

    Practiced leetcode 53 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,55 +1,39 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 1380
+        // leetcode 53
 
-        // Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
-        // Output: [15]
-        // Explanation: 15 is the only lucky number since it is the minimum in its row
-        // and the maximum in its column.
+        // Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+        // Output: 6
+        // Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
-        int[][] matrix = {
-                { 3, 7, 8 },
-                { 9, 11, 13 },
-                { 15, 16, 17 },
-        };
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 
-        System.out.println(luckyNumbers(matrix));
+        System.out.println(maxSubArray(nums));
 
     }
 
-    static public List<Integer> luckyNumbers(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
+    static public int maxSubArray(int[] nums) {
 
-        for (int i = 0; i < matrix.length; i++) {
-            int minRow = matrix[i][0];
-            int colIndex = 0;
+        int currentSum = 0;
+        int maxSum = nums[0];
 
-            for (int j = 1; j < matrix[i].length; j++) {
-                if (minRow > matrix[i][j]) {
-                    minRow = matrix[i][j];
-                    colIndex = j;
-                }
-            }
-            boolean isMax = true;
-            for (int j = 0; j < matrix.length; j++) {
-                if (minRow < matrix[j][colIndex]) {
-                    isMax = false;
-                    break;
-                }
-            }
-            if (isMax) {
-                result.add(minRow);
+        for (int i = 0; i < nums.length; i++) {
+            if (currentSum + nums[i] > nums[i]) {
+                currentSum = currentSum + nums[i];
+            } else {
+                currentSum = nums[i];
             }
 
+            if (maxSum < currentSum) {
+                maxSum = currentSum;
+            }
         }
 
-        return result;
+        return maxSum;
+
     }
 
 }
