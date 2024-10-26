5c1304767e130cdd5ec01212901280f44bdd3324
Kishore <kishorework77@gmail.com>
2024-10-26 10:33:58 +0530

    practiced leetcode 167 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,40 +1,40 @@
 package practice;
 
+import java.util.Arrays;
+
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 278
+        // leetcode 167
 
-        // Input: n = 5, bad = 4
-        // Output: 4
-        // Explanation:
-        // call isBadVersion(3) -> false
-        // call isBadVersion(5) -> true
-        // call isBadVersion(4) -> true
-        // Then 4 is the first bad version.
+        // Input: numbers = [2,7,11,15], target = 9
+        // Output: [1,2]
+        // Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We
+        // return [1, 2].
 
+        int[] numbers = { 2, 7, 11, 15 };
+        System.out.println(Arrays.toString(twoSum(numbers, 9)));
     }
 
-    static public int firstBadVersion(int n) {
-        int start = 1;
-        int end = n;
-        int badVersion = 1;
+    static public int[] twoSum(int[] numbers, int target) {
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (isBadVersion(mid)) {
-                end = mid - 1;
-                badVersion = mid;
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+
+            if (sum == target) {
+                return new int[] { start + 1, end + 1 };
+            } else if (sum > target) {
+                end--;
             } else {
-                start = mid + 1;
+                start++;
             }
         }
-        return badVersion;
-    }
 
-    // this is a predefined api fn for the problem
-    static public boolean isBadVersion(int num) {
-        return false;
+        return new int[] { -1, -1 };
+
     }
 
 }
