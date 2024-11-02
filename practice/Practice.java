7885d08eda7bed6facdc28cd2f122d03698bc83f
Kishore <kishorework77@gmail.com>
2024-11-02 20:34:47 +0530

    practiced leetcode 852 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,31 +3,35 @@ package practice;
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 35
+        // leetcode 852
 
-        // Input: nums = [1,3,5,6], target = 5
-        // Output: 2
+        // Input: arr = [0,1,0]
 
-        int[] nums = { 1, 3, 5, 6 };
-        System.out.println(searchInsert(nums, 2));
+        // Output: 1
+
+        int[] nums = { 0, 1, 0 };
+
+        System.out.println(peakIndexInMountainArray(nums));
+
     }
 
-    static public int searchInsert(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+    static public int peakIndexInMountainArray(int[] arr) {
 
-        while (start <= end) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
             int mid = start + (end - start) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
+            if (arr[mid] < arr[mid + 1]) {
                 start = mid + 1;
             } else {
-                end = mid - 1;
+                end = mid;
             }
         }
-        return start;
+
+        return end;
+
     }
 
 }
