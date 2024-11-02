cf4c526033908c077c3e91ce845525309cc40f22
Kishore <kishorework77@gmail.com>
2024-11-02 15:45:36 +0530

    practiced leetcode 35 in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,36 +3,31 @@ package practice;
 class Practice {
     public static void main(String[] args) {
 
-        // leetcode 1539
+        // leetcode 35
 
-        // Input: arr = [2,3,4,7,11], k = 5
-        // Output: 9
-        // Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The
-        // 5th missing positive integer is 9.
+        // Input: nums = [1,3,5,6], target = 5
+        // Output: 2
 
-        int[] arr = { 2, 3, 4, 7, 11 };
-
-        System.out.println(findKthPositive(arr, 5));
+        int[] nums = { 1, 3, 5, 6 };
+        System.out.println(searchInsert(nums, 2));
     }
 
-    static public int findKthPositive(int[] arr, int k) {
-
+    static public int searchInsert(int[] nums, int target) {
         int start = 0;
-        int end = arr.length - 1;
+        int end = nums.length - 1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            int missing = arr[mid] - (mid + 1);
 
-            if (missing < k) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-
-        return start + k;
-
+        return start;
     }
 
 }
