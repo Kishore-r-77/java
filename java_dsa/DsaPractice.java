020afe4237530c5c4178e4d69af652b1d50e0ed4
Kishore <kishorework77@gmail.com>
2024-11-09 17:24:58 +0530

    practiced selection sort in java

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -7,27 +7,34 @@ public class DsaPractice {
 	public static void main(String[] args) {
 		int[] arr = { 5, 4, 3, 2, 1 };
 
-		bubbleSort(arr);
+		selectionSort(arr);
 
 		System.out.println(Arrays.toString(arr));
 	}
 
-	static void bubbleSort(int[] arr) {
+	static void selectionSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-			boolean swap = false;
-			for (int j = 1; j < arr.length - i; j++) {
-				if (arr[j] < arr[j - 1]) {
-					swap = true;
-					int temp = arr[j];
-					arr[j] = arr[j - 1];
-					arr[j - 1] = temp;
-				}
-			}
-			if (!swap) {
-				break;
-			}
-
+			int lastIndex = arr.length - i - 1;
+			int maxIndex = getMaxIndex(arr, 0, lastIndex);
+			swap(arr, maxIndex, lastIndex);
 		}
+	}
+
+	static int getMaxIndex(int arr[], int start, int end) {
+		int maxIndex = start;
+		for (int i = start; i <= end; i++) {
+			if (arr[i] > arr[maxIndex]) {
+				maxIndex = i;
+			}
+		}
+
+		return maxIndex;
+	}
+
+	static void swap(int[] arr, int start, int end) {
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
 	}
 
 }
