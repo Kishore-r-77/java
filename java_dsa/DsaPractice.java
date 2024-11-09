9c0abe19241e1658a8d1e578f0f90da23b2104d4
Kishore <kishorework77@gmail.com>
2024-11-09 18:35:39 +0530

    practiced insertion sort in java

--- java_dsa/DsaPractice.java
+++ java_dsa/DsaPractice.java
@@ -7,28 +7,21 @@ public class DsaPractice {
 	public static void main(String[] args) {
 		int[] arr = { 5, 4, 3, 2, 1 };
 
-		selectionSort(arr);
+		insertionSort(arr);
 
 		System.out.println(Arrays.toString(arr));
 	}
 
-	static void selectionSort(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			int lastIndex = arr.length - i - 1;
-			int maxIndex = getMaxIndex(arr, 0, lastIndex);
-			swap(arr, maxIndex, lastIndex);
-		}
-	}
-
-	static int getMaxIndex(int arr[], int start, int end) {
-		int maxIndex = start;
-		for (int i = start; i <= end; i++) {
-			if (arr[i] > arr[maxIndex]) {
-				maxIndex = i;
+	static void insertionSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j > 0; j--) {
+				if (arr[j] < arr[j - 1]) {
+					swap(arr, j, j - 1);
+				} else {
+					break;
+				}
 			}
 		}
-
-		return maxIndex;
 	}
 
 	static void swap(int[] arr, int start, int end) {
