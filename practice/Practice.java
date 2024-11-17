6e0043e8551e740dd11823927976d8fa452b14f7
Kishore <kishorework77@gmail.com>
2024-11-17 14:44:15 +0530

    practicec min max sum using recursion

--- practice/Practice.java
+++ practice/Practice.java
@@ -5,33 +5,43 @@ import java.util.Arrays;
 class Practice {
     public static void main(String[] args) {
 
-        // Input : A = {1, 2, 3, 4, 5}
-        // Output : [48]
-        // [20, 28]
-        // [8, 12, 16]
-        // [3, 5, 7, 9]
-        // [1, 2, 3, 4, 5]
+        // Input: arr = {1, 4, 3, -5, -4, 8, 6};
+        // Output: min = -5, max = 8
 
-        int[] nums = { 1, 2, 3, 4, 5 };
-        sumOfTriangle(nums);
+        int[] nums = { 1, 4, 3, -5, -4, 8, 6 };
+
+        System.out.println(Arrays.toString(minMaxRecursive(nums, 0, nums.length - 1)));
 
     }
 
-    static public void sumOfTriangle(int[] nums) {
+    static public int[] minMaxRecursive(int[] nums, int start, int end) {
 
-        int[] result = new int[nums.length - 1];
-        if (result.length < 1) {
-            return;
+        if (start == end) {
+            return new int[] { nums[start], nums[end] };
         }
 
-        for (int i = 0; i < result.length; i++) {
-            result[i] = nums[i] + nums[i + 1];
+        if (end - start == 1) {
+            return new int[] { minValue(nums[start], nums[end]), maxValue(nums[start], nums[end]) };
         }
 
-        System.out.println(Arrays.toString(result));
+        int mid = start + (end - start) / 2;
 
-        sumOfTriangle(result);
+        int[] leftArray = minMaxRecursive(nums, start, mid);
+        int[] rightArray = minMaxRecursive(nums, mid + 1, end);
 
+        int min = minValue(leftArray[0], rightArray[0]);
+        int max = maxValue(leftArray[1], rightArray[1]);
+
+        return new int[] { min, max };
+
+    }
+
+    static int maxValue(int val1, int val2) {
+        return val1 > val2 ? val1 : val2;
+    }
+
+    static int minValue(int val1, int val2) {
+        return val1 < val2 ? val1 : val2;
     }
 
 }
