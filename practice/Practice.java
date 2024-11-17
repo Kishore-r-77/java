20e6c2d4d006826341121639eb303a53feaf7813
Kishore <kishorework77@gmail.com>
2024-11-17 19:28:28 +0530

    practiced leetcode 509(corrected) in java

--- practice/Practice.java
+++ practice/Practice.java
@@ -15,7 +15,12 @@ class Practice {
     }
 
     static public int fib(int n) {
-
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
         int n1 = 0;
         int n2 = 1;
         int output = 0;
